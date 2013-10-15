58a3dc5b9ba365d16b7aaff6eca3b2c29da2031a
HazenBabcock <hbabcock@mac.com>
2013-10-15 16:10:32 -0400

    Refactoring the code to try and extract most of what is common to any object finder from that which is specific to a particular object finder.

--- 3d_daostorm/mufit_analysis.py
+++ 3d_daostorm/mufit_analysis.py
@@ -11,92 +11,12 @@ import random
 import subprocess
 import sys
 
-import sa_library.datareader as datareader
 import find_peaks
+import sa_library.datareader as datareader
 import sa_library.parameters as params
 import sa_library.readinsight3 as readinsight3
 import sa_library.writeinsight3 as writeinsight3
-
-
-src_dir = sys.path[0] + "/"
-
-
-#
-# Helper functions.
-#
-
-# Get "x" or "y" peak width versus z paremeters.
-def getWidthParams(parameters, which, for_mu_Zfit = False):
-    par = ["_wo", "_c", "_d", "A", "B", "C", "D"]
-    np_par = numpy.zeros(len(par))
-    for i,p in enumerate(par):
-        attr = "w" + which + p
-        if hasattr(parameters, attr):
-            np_par[i] = getattr(parameters, attr)
-    if for_mu_Zfit:
-        np_par[0] = np_par[0]/parameters.pixel_size
-        np_par[1] = np_par[1]*0.001
-        np_par[2] = np_par[2]*0.001
-#        for i in range(3):
-#            np_par[i] = np_par[i]*0.001
-    return np_par
-
-# Get z range
-def getZRange(parameters):
-    min_z = -0.5
-    max_z = 0.5
-    if hasattr(parameters, "min_z"):
-        min_z = parameters.min_z
-    if hasattr(parameters, "max_z"):
-        max_z = parameters.max_z
-    return [min_z, max_z]
-
-#
-# API functions.
-#
-
-# Averages all the molecules in a track into a single molecule.
-def averaging(mol_list_filename, ave_list_filename):
-    proc_params = [src_dir + "../sa_utilities/avemlist",
-                   mol_list_filename,
-                   ave_list_filename]
-    subprocess.call(proc_params)
-
-# Performs drift correction.
-def driftCorrection(list_files, parameters):
-    drift_path = src_dir + "../sa_utilities/"
-    drift_name = list_files[0][:-9] + "drift.txt"
-
-    # Check if we have been asked not to do z drift correction.
-    # The default is to do the correction.
-    z_correct = True
-    if hasattr(parameters, "z_correction"):
-        if not parameters.z_correction:
-            z_correct = False
-
-    if z_correct:
-        proc_params = ["python",
-                       drift_path + "xyz-drift-correction.py",
-                       list_files[0],
-                       drift_name,
-                       str(parameters.frame_step),
-                       str(parameters.d_scale)]
-    else:
-        proc_params = ["python",
-                       drift_path + "xyz-drift-correction.py",
-                       list_files[0],
-                       drift_name,
-                       str(parameters.frame_step),
-                       str(parameters.d_scale),
-                       str(1)]
-    subprocess.call(proc_params)
-
-    if (os.path.exists(drift_name)):
-        for list_file in list_files:
-            proc_params = [drift_path + "apply-drift-correction",
-                           list_file,
-                           drift_name]
-            subprocess.call(proc_params)
+import sa_utilities.std_analysis as std_analysis
 
 # Does the peak finding.
 def peakFinding(movie_file, mlist_file, parameters):
@@ -127,9 +47,9 @@ def peakFinding(movie_file, mlist_file, parameters):
 
     # process parameters
     if (parameters.model == "Z"):
-        wx_params = getWidthParams(parameters, "x", for_mu_Zfit = True)
-        wy_params = getWidthParams(parameters, "y", for_mu_Zfit = True)
-        [min_z, max_z] = getZRange(parameters)
+        wx_params = params.getWidthParams(parameters, "x", for_mu_Zfit = True)
+        wy_params = params.getWidthParams(parameters, "y", for_mu_Zfit = True)
+        [min_z, max_z] = params.getZRange(parameters)
 
         if(parameters.orientation == "inverted"):
             find_peaks.initZParams(wx_params, wy_params, min_z, max_z)
@@ -195,30 +115,6 @@ def peakFinding(movie_file, mlist_file, parameters):
         i3data.close()
         return 1
 
-# Does the frame-to-frame tracking.
-def tracking(mol_list_filename, parameters):
-    [min_z, max_z] = getZRange(parameters)
-    proc_params = [src_dir + "../sa_utilities/tracker",
-                   mol_list_filename,
-                   parameters.descriptor,
-                   str(parameters.radius),
-                   str(1000.0*min_z),
-                   str(1000.0*max_z)]
-    subprocess.call(proc_params)
-
-# Does z fitting.
-def zFitting(mol_list_filename, parameters):
-    if(parameters.orientation == "inverted"):
-        wx_str = map(str, getWidthParams(parameters, "y"))
-        wy_str = map(str, getWidthParams(parameters, "x"))
-    else:
-        wx_str = map(str, getWidthParams(parameters, "x"))
-        wy_str = map(str, getWidthParams(parameters, "y"))
-    proc_params = [src_dir + "../sa_utilities/fitz",
-                   mol_list_filename, 
-                   str(parameters.cutoff)] + wx_str + wy_str
-    subprocess.call(proc_params)
-
 
 # Peform analysis if called from the command line
 
@@ -235,41 +131,10 @@ if __name__ == "__main__":
         print "usage: <movie> <bin> <parameters.xml>"
         exit()
 
-    # peak finding
-    print "Peak finding"
-    if(not peakFinding(sys.argv[1], mlist_file, parameters)):
-        print ""
-        
-        # tracking
-        print "Tracking"
-        tracking(mlist_file, parameters)
-
-        # averaging
-        alist_file = None
-        if(parameters.radius > 0.0):
-            alist_file = mlist_file[:-9] + "alist.bin"
-            averaging(mlist_file, alist_file)
-            print ""
-
-        # z fitting
-        if parameters.do_zfit:
-            print "Fitting Z"
-            if alist_file:
-                zFitting(alist_file, parameters)
-            zFitting(mlist_file, parameters)
-            print ""
-
-        # drift correction
-        if hasattr(parameters, "drift_correction"):
-            if parameters.drift_correction:
-                print "Drift Correction"
-                if alist_file:
-                    driftCorrection([mlist_file, alist_file], parameters)
-                else:
-                    driftCorrection([mlist_file], parameters)
-                print ""
-    print "Analysis complete"
-
+    std_analysis.standardAnalysis(peakFinding,
+                                  sys.argv[1],
+                                  mlist_file,
+                                  parameters)
 
 #
 # The MIT License
