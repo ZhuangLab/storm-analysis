a158a2783b6bc2540a8cd748ec8d9f33d48243ca
Hazen Babcock <hbabcock@fas.harvard.edu>
2014-11-18 16:29:02 -0500

    Add compilation of L1H.

--- compile_all_linux.sh
+++ compile_all_linux.sh
@@ -29,12 +29,23 @@ gcc -fPIC -g -c -Wall -O3 scmos_utilities.c
 gcc -shared -Wl,-soname,scmos_utilities.so.1 -o scmos_utilities.so.1.0.1 scmos_utilities.o
 ln -s scmos_utilities.so.1.0.1 scmos_utilities.so
 
+cd ../L1H
+gcc -fPIC -g -c -Wall -O3 homotopy_common.c
+gcc -fPIC -g -c -Wall -O3 homotopy_general.c
+gcc -fPIC -g -c -Wall -O3 homotopy_storm.c
+gcc -fPIC -g -c -Wall -O3 homotopy_sse.c
+gcc -fPIC -g -c -Wall -O3 homotopy_imagea_common.c
+gcc -fPIC -g -c -Wall -O3 homotopy_imagea.c
 
-#./L1H/homotopy_common.c
-#./L1H/homotopy_imagea.c
-#./L1H/fista_lib.c
-#./L1H/homotopy_imagea_common.c
-#./L1H/homotopy_gpu.c
-#./L1H/homotopy_general.c
-#./L1H/homotopy_sse.c
-#./L1H/homotopy_storm.c
+gcc -shared -Wl,-soname,homotopy_general.so.1 -o homotopy_general.so.1.0.1 homotopy_general.o homotopy_common.o -llapack
+ln -s homotopy_general.so.1.0.1 homotopy_general.so
+gcc -shared -Wl,-soname,homotopy_storm.so.1 -o homotopy_storm.so.1.0.1 homotopy_storm.o homotopy_common.o -llapack
+ln -s homotopy_storm.so.1.0.1 homotopy_storm.so
+gcc -shared -Wl,-soname,homotopy_sse.so.1 -o homotopy_sse.so.1.0.1 homotopy_sse.o homotopy_common.o -llapack
+ln -s homotopy_sse.so.1.0.1 homotopy_sse.so
+
+gcc -shared -Wl,-soname,homotopy_ia_storm.so.1 -o homotopy_ia_storm.so.1.0.1 homotopy_imagea.o homotopy_storm.o homotopy_imagea_common.o homotopy_common.o -llapack
+ln -s homotopy_ia_storm.so.1.0.1 homotopy_ia_storm.so
+gcc -shared -Wl,-soname,homotopy_ia_sse.so.1 -o homotopy_ia_sse.so.1.0.1 homotopy_imagea.o homotopy_sse.o homotopy_imagea_common.o homotopy_common.o -llapack
+ln -s homotopy_ia_sse.so.1.0.1 homotopy_ia_sse.so
+
