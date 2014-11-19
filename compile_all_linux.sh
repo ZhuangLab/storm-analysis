07ce9d43d2882ff516929c4ae2817cfb87579aa4
Hazen Babcock <hbabcock@fas.harvard.edu>
2014-11-19 14:41:52 -0500

    Add note about compiling with SSE2 (or not..)

--- compile_all_linux.sh
+++ compile_all_linux.sh
@@ -33,7 +33,7 @@ cd ../L1H
 gcc -fPIC -g -c -Wall -O3 homotopy_common.c
 gcc -fPIC -g -c -Wall -O3 homotopy_general.c
 gcc -fPIC -g -c -Wall -O3 homotopy_storm.c
-gcc -fPIC -g -c -Wall -O3 homotopy_sse.c
+gcc -fPIC -g -c -Wall -O3 -msse2 homotopy_sse.c
 gcc -fPIC -g -c -Wall -O3 homotopy_imagea_common.c
 gcc -fPIC -g -c -Wall -O3 homotopy_imagea.c
 
