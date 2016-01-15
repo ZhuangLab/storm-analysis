36a6317a79480db7db15b4ebf0c9d686a4119d4e
Hazen Babcock <hbabcock@fas.harvard.edu>
2016-01-15 14:45:12 -0500

    Add 2D simulator. Make some minor tweaks to the 3D simulator.

--- simulator/simulate_2d.py
+++ simulator/simulate_2d.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# Generate simulated data (3D).
+# Generate simulated data (2D).
 #
 # Hazen 01/16
 #
@@ -12,22 +12,15 @@ import sys
 
 import sa_library.daxwriter as daxwriter
 import drawgaussians as dg
-import sa_library.i3dtype as i3dtype
-import sa_library.multi_fit_c as multi_fit_c
 import sa_library.writeinsight3 as writeinsight3
 
 if (len(sys.argv) != 5):
-    print "usage: <dax> <bin> <frames> <num_objects>"
+    print "usage: <dax> <bin> <number frames> <number objects>"
     exit()
 
-# Peak height.
+# Peak height and width.
+sigma = 1.0
 intensity = 500.0
-
-# Parameters for astigmatic PSF.
-wx_params = numpy.array([2.0,  0.150, 0.40, 0.0, 0.0])
-wy_params = numpy.array([2.0, -0.150, 0.40, 0.0, 0.0])
-
-sigma = multi_fit_c.calcSxSy(wx_params, wy_params, 0.0)[0]
 
 # Image size.
 x_size = 256
@@ -41,69 +34,42 @@ num_objects = int(sys.argv[4])
 for i in range(num_frames):
     print "Generating frame:", i
 
-    # Generate objects
-    sx_vals = numpy.zeros(num_objects)
-    sy_vals = numpy.zeros(num_objects)
-    z_vals = numpy.zeros(num_objects)
+    # Generate the objects.
     objects = numpy.zeros((num_objects,5))
     for j in range(num_objects):
-
-        # Random locations.
         if 1:
             x_off = 0.05 * float(x_size) + 0.9 * float(x_size) * random.random()
             y_off = 0.05 * float(y_size) + 0.9 * float(y_size) * random.random()
-            z_off = 0.8*(random.random() - 0.4)
-            z_vals[j] = z_off * 1000.0
-            #i_val = random.expovariate(1.0/intensity)
-            i_val = intensity
-            [sx, sy] = multi_fit_c.calcSxSy(wx_params, wy_params, z_off)
-            sx_vals[j] = sx
-            sy_vals[j] = sy
-            objects[j,:] = [x_off,
-                            y_off,
-                            i_val,
-                            sx,
-                            sy]
-
-        # On a grid.
-        if 0:
-            x_off = 10.0 + 10.0*(j%23)
-            y_off = 10.0 + 10.0*math.floor(float(j)/23.0)
-            #x_off = 10.0 + 10.0*(j%3)
-            #y_off = 10.0 + 10.0*math.floor(float(j)/3.0)
-            #z_off = -0.5 + 1.0*y_off/float(y_size)
-            z_off = 0.0
-            z_vals[j] = z_off * 1000.0
-            [sx, sy] = multi_fit_c.calcSxSy(wx_params, wy_params, z_off)
-            sx_vals[j] = sx
-            sy_vals[j] = sy
             objects[j,:] = [x_off,
                             y_off,
                             intensity,
-                            sx,
-                            sy]
+                            sigma,
+                            sigma]
+            
+        if 0:
+            x_off = 10.0 + 10.0*(j%23) + 2.0*i
+            y_off = 10.0 + 10.0*math.floor(float(j)/23.0)
+            objects[j,:] = [x_off,
+                            y_off,
+                            intensity,
+                            sigma,
+                            sigma]
 
     # Draw the image.
     image = dg.drawGaussians([x_size, y_size], objects, background = 200, res = 5)
 
     # Add poisson noise and baseline.
     image = numpy.random.poisson(image) + 100.0
-        
-    # Save the image.
+    
+    # Save the image
     dax_data.addFrame(image)
 
     # Save the molecule locations.
-    ax = sy_vals/sx_vals
-    ww = 2.0*160.0*numpy.sqrt(sx_vals*sy_vals)
-    mols = i3dtype.createDefaultI3Data(num_objects)
-    i3dtype.posSet(mols, 'x', objects[:,0] + 1.0)
-    i3dtype.posSet(mols, 'y', objects[:,1] + 1.0)
-    i3dtype.posSet(mols, 'z', z_vals)
-    i3dtype.setI3Field(mols, 'a', objects[:,2])
-    i3dtype.setI3Field(mols, 'w', ww)
-    i3dtype.setI3Field(mols, 'ax', ax)
-    i3dtype.setI3Field(mols, 'fr', i+1)
-    i3_data.addMolecules(mols)
+    frame = (i+1)*numpy.ones(objects.shape[0])
+    i3_data.addMoleculesWithXYIFrame(objects[:,0] + 1.0,
+                                     objects[:,1] + 1.0,
+                                     objects[:,2],
+                                     frame)
 
 dax_data.close()
 i3_data.close()
