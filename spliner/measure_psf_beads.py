9cf819616b6e756b297ee94adf89a954cd53664f
Hazen Babcock <hbabcock@fas.harvard.edu>
2016-03-07 16:58:44 -0500

    Multiply PSF by a larger amount before saving as a .dax file.

--- spliner/measure_psf_beads.py
+++ spliner/measure_psf_beads.py
@@ -129,7 +129,7 @@ if 1:
     import sa_library.daxwriter as daxwriter
     dxw = daxwriter.DaxWriter("psf_beads.dax", average_psf.shape[1], average_psf.shape[2])
     for i in range(max_z):
-        dxw.addFrame(1000.0 * average_psf[i,:,:] + 100)
+        dxw.addFrame(10000.0 * average_psf[i,:,:] + 100)
     dxw.close()
 
 # Save PSF. 
