a9927af2069ad3b07dc73e56fb2d47235f6eee5b
Hazen Babcock <hbabcock@fas.harvard.edu>
2016-07-27 16:31:40 -0400

    Update to work for the 2016 challenge.

--- sa_utilities/bin_to_lmchallenge_format.py
+++ sa_utilities/bin_to_lmchallenge_format.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python
 #
+# 03/14
+#
 # Convert a .bin format file to the Single Molecule
 # Localization Challenge format.
 #
-# Hazen 03/14
+# 07/16
+#
+# Updated for the 2016 challenge.
+# 
+# Hazen
 #
 
 import sys
@@ -18,33 +24,39 @@ i3_reader = readinsight3.I3Reader(sys.argv[1])
 i3_block = i3_reader.nextBlock(block_size = 1000, good_only = False)
 
 smlc_file_fp = open(sys.argv[2], "w")
-smlc_file_fp.write("frame,xnano,ynano,intensity\n")
+smlc_file_fp.write("index, frame, xnano, ynano, znano, intensity\n")
 
 pix_to_nm = float(sys.argv[3])
 
 print "Saving Localizations"
 localization_number = 0
+index = 0
 while (type(i3_block) != type(False)):
 
     print " saving localization", localization_number
 
     for i in range(len(i3_block)):
-        fr = i3_block['fr'][i]
-        yp = (i3_block['xc'][i] - 0.5) * pix_to_nm
-        xp = (i3_block['yc'][i] - 0.5) * pix_to_nm
-        intensity = i3_block['a'][i]
+        track_length = i3_block['tl'][i]
+        for j in range(track_length):
+            fr = i3_block['fr'][i] + j
+            xp = i3_block['xc'][i] * pix_to_nm
+            yp = i3_block['yc'][i] * pix_to_nm
+            zp = i3_block['zc'][i]
+            intensity = i3_block['a'][i]
 
-        smlc_file_fp.write("{0:d}, {1:.3f}, {2:.3f}, {3:.3f}\n".format(fr, xp, yp, intensity))
+            index += 1
+            smlc_file_fp.write("{0:d}, {1:d}, {2:.3f}, {3:.3f}, {4:.3f}, {5:.3f}\n".format(index, fr, xp, yp, zp, intensity))
 
     localization_number += len(i3_block)
     i3_block = i3_reader.nextBlock(block_size = 1000, good_only = False)
 
+print "Saved", index, "molecules."
 smlc_file_fp.close()
 
 #
 # The MIT License
 #
-# Copyright (c) 2014 Zhuang Lab, Harvard University
+# Copyright (c) 2016 Zhuang Lab, Harvard University
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
