b29044f83ee2cf0b68e8e1a51654526c50ff587a
Hazen Babcock <hbabcock@fas.harvard.edu>
2016-08-04 10:58:37 -0400

    Add Windows DLLs for spliner. Update batch file for mingw compilation.

--- spliner/compile_bat.bat
+++ spliner/compile_bat.bat
@@ -1,6 +1,5 @@
-C:\MinGW64\bin\x86_64-w64-mingw32-gcc -c -O3 cubic_spline.c
-C:\MinGW64\bin\x86_64-w64-mingw32-gcc -c -O3 multi_fit_core.c
-C:\MinGW64\bin\x86_64-w64-mingw32-gcc -c -O3 cubic_fit.c
-C:\MinGW64\bin\x86_64-w64-mingw32-gcc -shared -o cubic_spline.dll cubic_spline.o
-C:\MinGW64\bin\x86_64-w64-mingw32-gcc -shared -o cubic_fit.dll cubic_fit.o multi_fit_core.o cubic_spline.o -llapack -Lc:\Users\Hazen\lib
-
+C:\MinGW64\bin\gcc -c -O3 cubic_spline.c
+C:\MinGW64\bin\gcc -c -O3 multi_fit_core.c
+C:\MinGW64\bin\gcc -c -O3 cubic_fit.c
+C:\MinGW64\bin\gcc -shared -o cubic_spline.dll cubic_spline.o
+C:\MinGW64\bin\gcc -shared -o cubic_fit.dll cubic_fit.o multi_fit_core.o cubic_spline.o -llapack -Lc:\Users\Hazen\storm-analysis\windows_dll
