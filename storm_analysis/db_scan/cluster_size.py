45ea4f269efdace8f44e6bc69431a1ff70d5836e
Hazen Babcock <hbabcock@fas.harvard.edu>
2016-09-12 09:07:43 -0400

    import C library as dbscanC not dbscan_c

--- storm_analysis/db_scan/cluster_size.py
+++ storm_analysis/db_scan/cluster_size.py
@@ -10,7 +10,7 @@
 
 import sys
 
-import lib.dbscan_c as dbscan_c
+import lib.dbscan_c as dbscanC
 import storm_analysis.sa_library.i3dtype as i3dtype
 import storm_analysis.sa_library.readinsight3 as readinsight3
 import storm_analysis.sa_library.writeinsight3 as writeinsight3
@@ -19,14 +19,14 @@ import storm_analysis.sa_library.writeinsight3 as writeinsight3
 i3_data_in = readinsight3.loadI3GoodOnly(sys.argv[1])
 
 # Remove category zero localizations.
-if 0:
+if False:
     print "warning, removing category zero localizations!"
     i3_data = i3dtype.maskData(i3_data_in, (i3_data_in['c'] != 0))
 else:
     i3_data =  i3_data_in
 
 # Record cluster localization numbers in the fit area field.
-i3_data['a'] = dbscan_c.localizationClusterSize(i3_data['lk'])+1
+i3_data['a'] = dbscanC.localizationClusterSize(i3_data['lk'])+1
 
 # Copy cluster id into the frame field.
 i3_data['fr'] = i3_data['lk']
