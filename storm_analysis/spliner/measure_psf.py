79580bf0bc05ee6ce5240b647815ad1b9b9790b5
Hazen Babcock <hbabcock@fas.harvard.edu>
2016-10-03 11:51:19 -0400

    Pull updated files from master branch.

--- storm_analysis/spliner/measure_psf.py
+++ storm_analysis/spliner/measure_psf.py
@@ -41,7 +41,8 @@ if (len(sys.argv)!= 6):
     exit()
 
 # Half width of the aoi size in pixels.
-aoi_size = 16
+aoi_size = 12
+#aoi_size = 16
 
 # Load dax file, z offset file and molecule list file.
 dax_data = datareader.inferReader(sys.argv[1])
@@ -193,26 +194,3 @@ else:
 
 pickle.dump(psf_dict, open(sys.argv[4], 'wb'))
 
-#
-# The MIT License
-#
-# Copyright (c) 2016 Zhuang Lab, Harvard University
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-#
