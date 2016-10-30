89a1f757c9b4bf96cf1bd9fb76bee31ed960e1f1
Hazen Babcock <hbabcock@mac.com>
2016-10-29 23:59:44 -0400

    Get spliner working with the new testing framework.

--- storm_analysis/spliner/psf_to_spline.py
+++ storm_analysis/spliner/psf_to_spline.py
@@ -18,89 +18,103 @@ import storm_analysis.spliner.spline1D as spline1D
 import storm_analysis.spliner.spline2D as spline2D
 import storm_analysis.spliner.spline3D as spline3D
 
-if (len(sys.argv)!=4):
-    print("usage: psf_to_spline <psf file, input> <spline file, output> <size, input>")
-    exit()
 
-psf_data = pickle.load(open(sys.argv[1], 'rb'))
-np_psf = psf_data["psf"]
-s_size = int(sys.argv[3])
-spline = False
-start = np_psf.shape[1]/2.0 - s_size - 0.5
+def psfToSpline(psf_name, spline_name, s_size):
+    
+    psf_data = pickle.load(open(psf_name, 'rb'))
+    np_psf = psf_data["psf"]
+    spline = False
+    start = np_psf.shape[1]/2.0 - s_size - 0.5
 
 
-# 2D spline
-if (len(np_psf.shape) == 2):
-    print("Generating 2D spline.")
-    s_size = 2*s_size
+    # 2D spline
+    if (len(np_psf.shape) == 2):
+        print("Generating 2D spline.")
+        s_size = 2*s_size
 
-    np_spline = numpy.zeros((s_size, s_size))
-    #np_psf = np_psf/numpy.max(np_psf)
-    xy_spline = spline2D.Spline2D(np_psf)
-
-    x = start
-    for i in range(s_size):
-        y = start
-        for j in range(s_size):
-            np_spline[j,i] = xy_spline.f(y,x)
-            
-            y += 1.0
-        x += 1.0
-
-    print("Calculating spline coefficients.")
-    spline = spline2D.Spline2D(np_spline)
-
-    if 1:
-        import sa_library.daxwriter as daxwriter
-        daxwriter.singleFrameDax("spline.dax", 1000.0*np_spline + 100)
-
-
-# 3D spline
-else:
-    print("Generating 3D spline.")
-    s_size = 2*s_size
-
-    np_spline = numpy.zeros((s_size, s_size, s_size))
-    xy_splines = []
-
-    print("Generating XY splines.")
-    for i in range(np_psf.shape[0]):
-        xy_splines.append(spline2D.Spline2D(np_psf[i,:,:]))
-
-    print("Generating fitting spline.")
-    x = start
-    for i in range(s_size):
-        y = start
-        for j in range(s_size):
-
-            zvals = numpy.zeros(np_psf.shape[0])
-            for k in range(np_psf.shape[0]):
-                zvals[k] = xy_splines[k].f(y,x)
-            z_spline = spline1D.Spline1D(zvals)
-
-            max_z = float(np_psf.shape[0]) - 1.0
-            inc = max_z/(float(s_size)-1.0)
-            for k in range(s_size):
-                z = float(k)*inc
-                if (z > max_z):
-                    z = max_z
-                np_spline[k,j,i] = z_spline.f(z)
-
-            y += 1.0
-        x += 1.0
-
-    print("Calculating spline coefficients.")
-    spline = spline3D.Spline3D(np_spline)
-
-    if 1:
-        import storm_analysis.sa_library.daxwriter as daxwriter
-        dxw = daxwriter.DaxWriter("spline.dax", np_spline.shape[1], np_spline.shape[2])
+        np_spline = numpy.zeros((s_size, s_size))
+        #np_psf = np_psf/numpy.max(np_psf)
+        xy_spline = spline2D.Spline2D(np_psf)
+        
+        x = start
         for i in range(s_size):
-            dxw.addFrame(1000.0*np_spline[i,:,:] + 100)
-        dxw.close()
+            y = start
+            for j in range(s_size):
+                np_spline[j,i] = xy_spline.f(y,x)
+            
+                y += 1.0
+            x += 1.0
 
-del psf_data["psf"]
-psf_data["spline"] = np_spline
-psf_data["coeff"] = spline.getCoeff()
-pickle.dump(psf_data, open(sys.argv[2], 'wb'))
+        print("Calculating spline coefficients.")
+        spline = spline2D.Spline2D(np_spline)
+
+        if True:
+            import sa_library.daxwriter as daxwriter
+            daxwriter.singleFrameDax("spline.dax", 1000.0*np_spline + 100)
+
+
+    # 3D spline
+    else:
+        print("Generating 3D spline.")
+        s_size = 2*s_size
+
+        np_spline = numpy.zeros((s_size, s_size, s_size))
+        xy_splines = []
+
+        print("Generating XY splines.")
+        for i in range(np_psf.shape[0]):
+            xy_splines.append(spline2D.Spline2D(np_psf[i,:,:]))
+
+        print("Generating fitting spline.")
+        x = start
+        for i in range(s_size):
+            y = start
+            for j in range(s_size):
+
+                zvals = numpy.zeros(np_psf.shape[0])
+                for k in range(np_psf.shape[0]):
+                    zvals[k] = xy_splines[k].f(y,x)
+                    z_spline = spline1D.Spline1D(zvals)
+
+                max_z = float(np_psf.shape[0]) - 1.0
+                inc = max_z/(float(s_size)-1.0)
+                for k in range(s_size):
+                    z = float(k)*inc
+                    if (z > max_z):
+                        z = max_z
+                    np_spline[k,j,i] = z_spline.f(z)
+
+                y += 1.0
+            x += 1.0
+
+        print("Calculating spline coefficients.")
+        spline = spline3D.Spline3D(np_spline)
+
+        if True:
+            import storm_analysis.sa_library.daxwriter as daxwriter
+            dxw = daxwriter.DaxWriter("spline.dax", np_spline.shape[1], np_spline.shape[2])
+            for i in range(s_size):
+                dxw.addFrame(1000.0*np_spline[i,:,:] + 100)
+            dxw.close()
+
+    del psf_data["psf"]
+    psf_data["spline"] = np_spline
+    psf_data["coeff"] = spline.getCoeff()
+    pickle.dump(psf_data, open(spline_name, 'wb'))
+
+
+if (__name__ == "__main__"):
+
+    import argparse
+    
+    parser = argparse.ArgumentParser(description = 'Convert PSF into a spline file.')
+
+    parser.add_argument('--psf', dest='psf', type=str, required=True)
+    parser.add_argument('--spline', dest='spline', type=str, required=True)
+    parser.add_argument('--spline_size', dest='spline_size', type=int, required=True)
+    
+    args = parser.parse_args()
+    
+    psfToSpline(args.psf, args.spline, args.spline_size)
+
 
