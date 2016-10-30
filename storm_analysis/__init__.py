03c7f975392c1395f7294925c100395cd44478ba
Hazen Babcock <hbabcock@mac.com>
2016-10-30 08:13:18 -0400

    Add function to remove test output files. Change function names to follow the projects 'convention', camel case for functions and classes, underscores for variables.

--- storm_analysis/__init__.py
+++ storm_analysis/__init__.py
@@ -2,18 +2,18 @@
 import os
 
 
-def get_data(data_path):
+def getData(data_path):
     import pkg_resources
     data = pkg_resources.resource_filename(__name__, data_path)
     return data
 
 
-def get_path(path):
+def getPath(path):
     return os.path.join(os.path.dirname(os.path.abspath(__file__)), path)
 
 
-def get_path_output_test(fname=None):
-    out_path = get_path("test/output/")
+def getPathOutputTest(fname=None):
+    out_path = getPath("test/output/")
     
     if not os.path.exists(out_path):
         os.makedirs(out_path)
@@ -22,3 +22,12 @@ def get_path_output_test(fname=None):
         return os.path.join(out_path, fname)
     else:
         return out_path
+
+
+def removeFile(fname):
+    try:
+        os.remove(fname)
+    except OSError:
+        pass
+
+    
