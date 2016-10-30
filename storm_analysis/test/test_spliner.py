dd783917dd5e5fe9ef62a5a3edc6e3bcff1959de
Hazen Babcock <hbabcock@mac.com>
2016-10-30 08:16:11 -0400

    Remove outputs.

--- storm_analysis/test/test_spliner.py
+++ storm_analysis/test/test_spliner.py
@@ -2,12 +2,12 @@
 
 import storm_analysis
 
-"""
 def test_measure_psf():
 
-    movie = storm_analysis.get_data("test/data/test_spliner.dax")
-    mlist = storm_analysis.get_data("test/data/test_spliner_olist.bin")
-    psf = storm_analysis.get_path_output_test("test_spliner_psf.psf")
+    movie = storm_analysis.getData("test/data/test_spliner.dax")
+    mlist = storm_analysis.getData("test/data/test_spliner_olist.bin")
+    psf = storm_analysis.getPathOutputTest("test_spliner_psf.psf")
+    storm_analysis.removeFile(psf)
 
     from storm_analysis.spliner.measure_psf import measurePSF
     measurePSF(movie, "", mlist, psf)
@@ -15,29 +15,31 @@ def test_measure_psf():
     
 def test_psf_to_spline():
 
-    psf = storm_analysis.get_path_output_test("test_spliner_psf.psf")
-    spline = storm_analysis.get_data("test/data/test_spliner_psf.spline")
-
+    psf = storm_analysis.getPathOutputTest("test_spliner_psf.psf")
+    spline = storm_analysis.getData("test/data/test_spliner_psf.spline")
+    storm_analysis.removeFile(spline)
+    
     from storm_analysis.spliner.psf_to_spline import psfToSpline
     psfToSpline(psf, spline, 10)
 
-"""
 def test_spliner_std():
 
-    movie_name = storm_analysis.get_data("test/data/test_spliner.dax")
-    settings = storm_analysis.get_data("test/data/test_spliner_dh.xml")
-    mlist = storm_analysis.get_path_output_test("test_spliner_slist.bin")
-
+    movie_name = storm_analysis.getData("test/data/test_spliner.dax")
+    settings = storm_analysis.getData("test/data/test_spliner_dh.xml")
+    mlist = storm_analysis.getPathOutputTest("test_spliner_slist.bin")
+    storm_analysis.removeFile(mlist)
+    
     from storm_analysis.spliner.spline_analysis import analyze
     analyze(movie_name, mlist, settings)
 
 
 def test_spliner_fista():
 
-    movie_name = storm_analysis.get_data("test/data/test_spliner.dax")
-    settings = storm_analysis.get_data("test/data/test_spliner_dh_fista.xml")
-    mlist = storm_analysis.get_path_output_test("test_spliner_flist.bin")
-
+    movie_name = storm_analysis.getData("test/data/test_spliner.dax")
+    settings = storm_analysis.getData("test/data/test_spliner_dh_fista.xml")
+    mlist = storm_analysis.getPathOutputTest("test_spliner_flist.bin")
+    storm_analysis.removeFile(mlist)
+    
     from storm_analysis.spliner.spline_analysis import analyze
     analyze(movie_name, mlist, settings)
     
