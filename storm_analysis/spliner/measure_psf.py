610c671a935fcae4a7ff41c6a0b144cd4f342172
Hazen Babcock <hbabcock@mac.com>
2016-10-29 23:35:57 -0400

    Make into a module.

--- storm_analysis/spliner/measure_psf.py
+++ storm_analysis/spliner/measure_psf.py
@@ -30,167 +30,176 @@ import numpy
 import os
 import scipy
 import scipy.ndimage
-import sys
 
 import storm_analysis.sa_library.ia_utilities_c as util_c
 import storm_analysis.sa_library.datareader as datareader
 import storm_analysis.sa_library.readinsight3 as readinsight3
 
-if (len(sys.argv)!= 6):
-    print("usage: measure_psf <dax_file, input> <z_file, input> <bin_file, input> <psf_file, output> <3d (0,1), input>")
-    exit()
 
-# Half width of the aoi size in pixels.
-aoi_size = 12
-#aoi_size = 16
+def measurePSF(movie_name, zfile_name, movie_mlist, psf_name, want2d = False, aoi_size = 12, z_range = 750.0, z_step = 50.0):
+    """
+    The actual z range is 2x z_range (i.e. from -z_range to z_range).
+    """
+    
+    # Load dax file, z offset file and molecule list file.
+    dax_data = datareader.inferReader(movie_name)
+    z_offset = None
+    if os.path.exists(zfile_name):
+        try:
+            z_offsets = numpy.loadtxt(zfile_name, ndmin = 2)[:,1]
+        except IndexError:
+            z_offsets = None
+            print("z offsets were not loaded.")
+    i3_data = readinsight3.loadI3File(movie_mlist)
 
-# Load dax file, z offset file and molecule list file.
-dax_data = datareader.inferReader(sys.argv[1])
-z_offset = None
-if os.path.exists(sys.argv[2]):
-    try:
-        z_offsets = numpy.loadtxt(sys.argv[2], ndmin = 2)[:,1]
-    except IndexError:
-        z_offsets = None
-        print("z offsets were not loaded.")
-i3_data = readinsight3.loadI3File(sys.argv[3])
-
-# Determine whether this is 2D or 3D.
-analysis_type = "3D"
-if (sys.argv[5] == "0"):
-    print("Measuring 2D PSF")
-    analysis_type = "2D"
-else:
-    print("Measuring 3D PSF")
-
-#
-# Go through the frames identifying good peaks and adding them
-# to the average psf. For 3D molecule z positions are rounded to 
-# the nearest 50nm.
-#
-z_range = 750.0  # This is really more like the half range, the
-                 # full range will cover +- z_range.
-
-z_step = 50.0
-z_mid = int(z_range/z_step)
-max_z = 2 * z_mid + 1
-
-average_psf = numpy.zeros((max_z,4*aoi_size,4*aoi_size))
-peaks_used = 0
-totals = numpy.zeros(max_z)
-[dax_x, dax_y, dax_l] = dax_data.filmSize()
-for curf in range(dax_l):
-
-    # Select localizations in current frame & not near the edges.
-    mask = (i3_data['fr'] == curf+1) & (i3_data['x'] > aoi_size) & (i3_data['x'] < (dax_x - aoi_size - 1)) & (i3_data['y'] > aoi_size) & (i3_data['y'] < (dax_y - aoi_size - 1))
-    xr = i3_data['x'][mask]
-    yr = i3_data['y'][mask]
-
-    # Use the z offset file if it was specified, otherwise use localization z positions.
-    if z_offset is None:
-        if (curf == 0):
-            print("Using fit z locations.")
-        zr = i3_data['z'][mask]
+    if want2d:
+        print("Measuring 2D PSF")
     else:
-        if (curf == 0):
-            print("Using z offset file.")
-        zr = numpy.ones(xr.size) * z_offsets[curf]
+        print("Measuring 3D PSF")
 
-    ht = i3_data['h'][mask]
+    #
+    # Go through the frames identifying good peaks and adding them
+    # to the average psf. For 3D molecule z positions are rounded to 
+    # the nearest 50nm.
+    #
+    z_mid = int(z_range/z_step)
+    max_z = 2 * z_mid + 1
 
-    # Remove localizations that are too close to each other.
-    in_peaks = numpy.zeros((xr.size,util_c.getNResultsPar()))
-    in_peaks[:,util_c.getXCenterIndex()] = xr
-    in_peaks[:,util_c.getYCenterIndex()] = yr
-    in_peaks[:,util_c.getZCenterIndex()] = zr
-    in_peaks[:,util_c.getHeightIndex()] = ht
+    average_psf = numpy.zeros((max_z,4*aoi_size,4*aoi_size))
+    peaks_used = 0
+    totals = numpy.zeros(max_z)
+    [dax_x, dax_y, dax_l] = dax_data.filmSize()
+    for curf in range(dax_l):
 
-    out_peaks = util_c.removeNeighbors(in_peaks, 2*aoi_size)
-    #out_peaks = util_c.removeNeighbors(in_peaks, aoi_size)
+        # Select localizations in current frame & not near the edges.
+        mask = (i3_data['fr'] == curf+1) & (i3_data['x'] > aoi_size) & (i3_data['x'] < (dax_x - aoi_size - 1)) & (i3_data['y'] > aoi_size) & (i3_data['y'] < (dax_y - aoi_size - 1))
+        xr = i3_data['x'][mask]
+        yr = i3_data['y'][mask]
 
-    print(curf, "peaks in", in_peaks.shape[0], ", peaks out", out_peaks.shape[0])
-
-    # Use remaining localizations to calculate spline.
-    image = dax_data.loadAFrame(curf).astype(numpy.float64)
-
-    xr = out_peaks[:,util_c.getXCenterIndex()]
-    yr = out_peaks[:,util_c.getYCenterIndex()]
-    zr = out_peaks[:,util_c.getZCenterIndex()]
-    ht = out_peaks[:,util_c.getHeightIndex()]
-
-    for i in range(xr.size):
-        xf = xr[i]
-        yf = yr[i]
-        zf = zr[i]
-        xi = int(xf)
-        yi = int(yf)
-        if (analysis_type == "2D"):
-            zi = 0
+        # Use the z offset file if it was specified, otherwise use localization z positions.
+        if z_offset is None:
+            if (curf == 0):
+                print("Using fit z locations.")
+            zr = i3_data['z'][mask]
         else:
-            zi = int(round(zf/z_step) + z_mid)
+            if (curf == 0):
+                print("Using z offset file.")
+            zr = numpy.ones(xr.size) * z_offsets[curf]
 
-        # check the z is in range
-        if (zi > -1) and (zi < max_z):
+        ht = i3_data['h'][mask]
 
-            # get localization image
-            mat = image[xi-aoi_size:xi+aoi_size,
-                        yi-aoi_size:yi+aoi_size]
+        # Remove localizations that are too close to each other.
+        in_peaks = numpy.zeros((xr.size,util_c.getNResultsPar()))
+        in_peaks[:,util_c.getXCenterIndex()] = xr
+        in_peaks[:,util_c.getYCenterIndex()] = yr
+        in_peaks[:,util_c.getZCenterIndex()] = zr
+        in_peaks[:,util_c.getHeightIndex()] = ht
 
-            # zoom in by 2x
-            psf = scipy.ndimage.interpolation.zoom(mat, 2.0)
+        out_peaks = util_c.removeNeighbors(in_peaks, 2*aoi_size)
+        #out_peaks = util_c.removeNeighbors(in_peaks, aoi_size)
 
-            # re-center image
-            psf = scipy.ndimage.interpolation.shift(psf, (-2.0*(xf-xi), -2.0*(yf-yi)), mode='nearest')
+        print(curf, "peaks in", in_peaks.shape[0], ", peaks out", out_peaks.shape[0])
 
-            # add to average psf accumulator
-            average_psf[zi,:,:] += psf
-            totals[zi] += 1
+        # Use remaining localizations to calculate spline.
+        image = dax_data.loadAFrame(curf).astype(numpy.float64)
 
-# Force PSF to be zero (on average) at the boundaries.
-for i in range(max_z):
-    edge = numpy.concatenate((average_psf[i,0,:],
-                              average_psf[i,-1,:],
-                              average_psf[i,:,0],
-                              average_psf[i,:,-1]))
-    average_psf[i,:,:] -= numpy.mean(edge)
+        xr = out_peaks[:,util_c.getXCenterIndex()]
+        yr = out_peaks[:,util_c.getYCenterIndex()]
+        zr = out_peaks[:,util_c.getZCenterIndex()]
+        ht = out_peaks[:,util_c.getHeightIndex()]
 
-# Normalize the PSF.
-if (analysis_type == "2D"):
-    max_z = 1
+        for i in range(xr.size):
+            xf = xr[i]
+            yf = yr[i]
+            zf = zr[i]
+            xi = int(xf)
+            yi = int(yf)
+            if want2d:
+                zi = 0
+            else:
+                zi = int(round(zf/z_step) + z_mid)
 
-for i in range(max_z):
-    print(i, totals[i])
-    if (totals[i] > 0.0):
-        average_psf[i,:,:] = average_psf[i,:,:]/numpy.sum(numpy.abs(average_psf[i,:,:]))
+            # check the z is in range
+            if (zi > -1) and (zi < max_z):
 
-average_psf = average_psf/numpy.max(average_psf)
+                # get localization image
+                mat = image[xi-aoi_size:xi+aoi_size,
+                            yi-aoi_size:yi+aoi_size]
 
-# Save PSF (in image form).
-if 1:
-    import storm_analysis.sa_library.daxwriter as daxwriter
-    dxw = daxwriter.DaxWriter("psf.dax", average_psf.shape[1], average_psf.shape[2])
+                # zoom in by 2x
+                psf = scipy.ndimage.interpolation.zoom(mat, 2.0)
+
+                # re-center image
+                psf = scipy.ndimage.interpolation.shift(psf, (-2.0*(xf-xi), -2.0*(yf-yi)), mode='nearest')
+
+                # add to average psf accumulator
+                average_psf[zi,:,:] += psf
+                totals[zi] += 1
+
+    # Force PSF to be zero (on average) at the boundaries.
     for i in range(max_z):
-        dxw.addFrame(1000.0 * average_psf[i,:,:] + 100)
-    dxw.close()
+        edge = numpy.concatenate((average_psf[i,0,:],
+                                  average_psf[i,-1,:],
+                                  average_psf[i,:,0],
+                                  average_psf[i,:,-1]))
+        average_psf[i,:,:] -= numpy.mean(edge)
 
-# Save PSF.
-if (analysis_type == "2D"):
-    dict = {"psf" : average_psf[0,:,:],
-            "type" : "2D"}
+    # Normalize the PSF.
+    if want2d:
+        max_z = 1
 
-else:
-    cur_z = -z_range
-    z_vals = []
     for i in range(max_z):
-        z_vals.append(cur_z)
-        cur_z += z_step
+        print(i, totals[i])
+        if (totals[i] > 0.0):
+            average_psf[i,:,:] = average_psf[i,:,:]/numpy.sum(numpy.abs(average_psf[i,:,:]))
 
-    psf_dict = {"psf" : average_psf,
-                "pixel_size" : 0.080, # 1/2 the camera pixel size in nm.
-                "type" : "3D",
-                "zmin" : -z_range,
-                "zmax" : z_range,
-                "zvals" : z_vals}
+    average_psf = average_psf/numpy.max(average_psf)
 
-pickle.dump(psf_dict, open(sys.argv[4], 'wb'))
+    # Save PSF (in image form).
+    if True:
+        import storm_analysis.sa_library.daxwriter as daxwriter
+        dxw = daxwriter.DaxWriter("psf.dax", average_psf.shape[1], average_psf.shape[2])
+        for i in range(max_z):
+            dxw.addFrame(1000.0 * average_psf[i,:,:] + 100)
+        dxw.close()
 
+    # Save PSF.
+    if want2d:
+        dict = {"psf" : average_psf[0,:,:],
+                "type" : "2D"}
+
+    else:
+        cur_z = -z_range
+        z_vals = []
+        for i in range(max_z):
+            z_vals.append(cur_z)
+            cur_z += z_step
+
+        psf_dict = {"psf" : average_psf,
+                    "pixel_size" : 0.080, # 1/2 the camera pixel size in nm.
+                    "type" : "3D",
+                    "zmin" : -z_range,
+                    "zmax" : z_range,
+                    "zvals" : z_vals}
+
+    pickle.dump(psf_dict, open(psf_name, 'wb'))
+
+
+if (__name__ == "__main__"):
+
+    import argparse
+    
+    parser = argparse.ArgumentParser(description = 'Measure PSF given a movie, a list.bin file and (optionally) a z_offset file')
+
+    parser.add_argument('--movie', dest='movie', type=str, required=True)
+    parser.add_argument('--bin', dest='mlist', type=str, required=True)
+    parser.add_argument('--psf', dest='psf', type=str, required=True)
+    parser.add_argument('--zoffset', dest='zoffset', type=str, required=False, default="")
+    parser.add_argument('--want2d', dest='want2d', type=bool, required=False, default=False)
+    parser.add_argument('--aoi_size', dest='aoi_size', type=int, required=False, default=12)
+    parser.add_argument('--zrange', dest='zrange', type=float, required=False, default=750)
+    parser.add_argument('--zstep', dest='zstep', type=float, required=False, default=50)
+
+    args = parser.parse_args()
+
+    measurePSF(args.movie_name, args.zoffset, args.mlist, args.psf, want2d = args.want2d, aoi_size = args.aoi_size, z_range = args.zrange, z_step = args.z_step)
