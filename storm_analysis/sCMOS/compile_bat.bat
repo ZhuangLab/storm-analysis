82972029b3af114be044035f643f52a1d6a29dfc
Hazen Babcock <hbabcock@mac.com>
2016-11-12 17:25:14 -0500

    Remove old DLLs and compilation shell scripts.

--- storm_analysis/sCMOS/compile_bat.bat
+++
@@ -1,2 +0,0 @@
-C:\MinGW64\bin\x86_64-w64-mingw32-gcc -c -O3 scmos_utilities.c
-C:\MinGW64\bin\x86_64-w64-mingw32-gcc -shared -o scmos_utilities.dll scmos_utilities.o
--- storm_analysis/sa_library/compile_bat.bat
+++
@@ -1,7 +0,0 @@
-C:\MinGW64\bin\x86_64-w64-mingw32-gcc -c -O3 grid.c
-C:\MinGW64\bin\x86_64-w64-mingw32-gcc -c -O3 ia_utilities.c
-C:\MinGW64\bin\x86_64-w64-mingw32-gcc -c -O3 multi_fit.c
-C:\MinGW64\bin\x86_64-w64-mingw32-gcc -shared -o grid.dll grid.o
-C:\MinGW64\bin\x86_64-w64-mingw32-gcc -shared -o ia_utilities.dll ia_utilities.o
-C:\MinGW64\bin\x86_64-w64-mingw32-gcc -shared -o multi_fit.dll multi_fit.o -llapack -Lc:\Users\Hazen\lib
-
