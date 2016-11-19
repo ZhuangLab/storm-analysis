ff7e226d195cb9e9aeb8334cb3e9f9452b4783da
Hazen Babcock <hbabcock@mac.com>
2016-11-19 15:29:37 -0500

    Add custom build command for building the C libraries. Call this command at the build_py stage so that the C libraries get built.

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+# Custom commands following:
+#  https://seasonofcode.com/posts/how-to-add-custom-build-steps-and-commands-to-setuppy.html
+#
 
+import distutils.cmd
 import platform
 import os
+import setuptools.command.build_py
+import subprocess
 import sys
 
 from setuptools import setup, find_packages
@@ -10,109 +17,45 @@ from distutils.core import Extension
 
 import numpy
 
+
 version = "1.0"
-description = "Zhuang lab STORM movie analysis code."
+description = "STORM movie analysis code."
 long_description = ""
 
-def get_c_extensions():
+class SConsCommand(distutils.cmd.Command):
+    """
+    Custom command to run scons (http://scons.org/) to build the C libraries.
+    """
+    description = 'run scons to build C libraries'
+    user_options = [
+        ('scons-exe', None, 'location of the scons executable'),
+    ]
 
-    include_dirs = [os.path.join(sys.prefix, "include")]
-    library_dirs = []
+    def initialize_options(self):
+        self.scons_exe = ''
 
-    if platform.system() == 'Windows':
-        include_dirs += [os.environ['LIBRARY_INC']]
-        library_dirs += [os.environ['LIBRARY_LIB']]
-    elif platform.system() == 'Linux':
-        include_dirs += []
-        library_dirs += []
-    elif platform.system() == 'Darwin':
-        include_dirs += []
-        library_dirs += []
+    def finalize_options(self):
+        if self.scons_exe:
+            assert os.path.exists(self.scons_exe), ("scon executable " + self.scons_exe + " not found")
 
-    extensions = [Extension("storm_analysis.fista._fista_decon_utilities", ["./storm_analysis/fista/fista_decon_utilities.c"],
-                            libraries=library_dirs, include_dirs=include_dirs),
-                  Extension("storm_analysis.fista._fista_fft", ["./storm_analysis/fista/fista_fft.c"],
-                            libraries=library_dirs + ["fftw3"], include_dirs=include_dirs), 
+    def run(self):
+        if self.scons_exe:
+            command = [self.scons_exe]
+        else:
+            command = ['scons']
+        self.announce('Running command: ' + str(command))
+        subprocess.check_call(command)
 
-                  Extension("storm_analysis.sa_library._matched_filter", ["./storm_analysis/sa_library/matched_filter.c"],
-                            libraries=library_dirs + ["fftw3"], include_dirs=include_dirs),
-                  Extension("storm_analysis.sa_library._grid", ["./storm_analysis/sa_library/grid.c"],
-                            libraries=library_dirs, include_dirs=include_dirs),
-                  Extension("storm_analysis.sa_library._dao_fit", ["./storm_analysis/sa_library/dao_fit.c",
-                                                                   "./storm_analysis/sa_library/multi_fit.c"],
-                            libraries=library_dirs + ["lapack"], include_dirs=include_dirs),
-                  Extension("storm_analysis.sa_library._ia_utilities", ["./storm_analysis/sa_library/ia_utilities.c"],
-                            libraries=library_dirs, include_dirs=include_dirs),
 
-                  Extension("storm_analysis.dbscan._dbscan", ["./storm_analysis/dbscan/kdtree.c",
-                                                              "./storm_analysis/dbscan/dbscan.c"],
-                            libraries=library_dirs, include_dirs=include_dirs),
+class BuildPyCommand(setuptools.command.build_py.build_py):
+  """
+  Custom build command so that the C libraries also get built.
+  """
 
-                  Extension("storm_analysis.decon_storm._mlem_sparse", ["./storm_analysis/decon_storm/mlem_sparse.c"],
-                            libraries=library_dirs, include_dirs=include_dirs),
-
-                  Extension("storm_analysis.sCMOS._scmos_utilities", ["./storm_analysis/sCMOS/scmos_utilities.c"],
-                            libraries=library_dirs, include_dirs=include_dirs),
-  
-                  Extension("storm_analysis.frc._frc", ["./storm_analysis/frc/frc.c"],
-                            libraries=library_dirs, include_dirs=include_dirs),
-
-                  Extension("storm_analysis.simulator._draw_gaussians", ["./storm_analysis/simulator/draw_gaussians.c"],
-                            libraries=library_dirs, include_dirs=include_dirs),
-                  Extension("storm_analysis.simulator._zernike", ["./storm_analysis/simulator/zernike.c"],
-                            libraries=library_dirs, include_dirs=include_dirs),
-
-                  Extension("storm_analysis.spliner._cubic_spline", ["./storm_analysis/spliner/cubic_spline.c"],
-                            libraries=library_dirs, include_dirs=include_dirs),
-                  Extension("storm_analysis.spliner._cubic_fit", ["./storm_analysis/spliner/cubic_fit.c",
-                                                                  "./storm_analysis/sa_library/multi_fit.c",
-                                                                  "./storm_analysis/spliner/cubic_spline.c"],
-                            libraries=library_dirs + ["lapack"], include_dirs=include_dirs),
-
-                  Extension("storm_analysis.rolling_ball_bgr._rolling_ball_lib", ["./storm_analysis/rolling_ball_bgr/rolling_ball_lib.c"],
-                            libraries=library_dirs, include_dirs=include_dirs),
-
-                  Extension("storm_analysis.sa_utilities._fitz", ["./storm_analysis/sa_utilities/fitz.c"],
-                            libraries=library_dirs, include_dirs=include_dirs),
-                  Extension("storm_analysis.sa_utilities._tracker", ["./storm_analysis/sa_utilities/tracker.c"],
-                            libraries=library_dirs, include_dirs=include_dirs),
-                  Extension("storm_analysis.sa_utilities._avemlist", ["./storm_analysis/sa_utilities/avemlist.c"],
-                            libraries=library_dirs, include_dirs=include_dirs),
-                  Extension("storm_analysis.sa_utilities._apply-drift-correction", ["./storm_analysis/sa_utilities/apply-drift-correction.c"],
-                            libraries=library_dirs, include_dirs=include_dirs),
-                  
-                  Extension("storm_analysis.L1H._homotopy_storm", ["./storm_analysis/L1H/homotopy_storm.c"],
-                            libraries=library_dirs, include_dirs=include_dirs),
-                  Extension("storm_analysis.L1H._homotopy_sse", ["./storm_analysis/L1H/homotopy_sse.c"],
-                            libraries=library_dirs, include_dirs=include_dirs),
-                  Extension("storm_analysis.L1H._homotopy_general", ["./storm_analysis/L1H/homotopy_general.c"],
-                            libraries=library_dirs, include_dirs=include_dirs),
-                  Extension("storm_analysis.L1H._homotopy_imagea", ["./storm_analysis/L1H/homotopy_imagea.c"],
-                            libraries=library_dirs, include_dirs=include_dirs),
-                  Extension("storm_analysis.L1H._homotopy_common", ["./storm_analysis/L1H/homotopy_common.c"],
-                            libraries=library_dirs, include_dirs=include_dirs),
-                  Extension("storm_analysis.L1H._homotopy_imagea_common", ["./storm_analysis/L1H/homotopy_imagea_common.c"],
-                            libraries=library_dirs, include_dirs=include_dirs),
-                  Extension("storm_analysis.L1H._homotopy_gpu", ["./storm_analysis/L1H/homotopy_gpu.c"],
-                            libraries=library_dirs, include_dirs=include_dirs),
-                  Extension("storm_analysis.L1H._homotopy_ia_storm", ["./storm_analysis/L1H/homotopy_imagea.c",
-                                                                      "./storm_analysis/L1H/homotopy_storm.c",
-                                                                      "./storm_analysis/L1H/homotopy_imagea_common.c",
-                                                                      "./storm_analysis/L1H/homotopy_common.c"],
-                            libraries=library_dirs + ["lapack"], include_dirs=include_dirs),
-                  Extension("storm_analysis.L1H._homotopy_ia_sse", ["./storm_analysis/L1H/homotopy_imagea.c",
-                                                                    "./storm_analysis/L1H/homotopy_sse.c",
-                                                                    "./storm_analysis/L1H/homotopy_imagea_common.c",
-                                                                    "./storm_analysis/L1H/homotopy_common.c"],
-                            libraries=library_dirs + ["lapack"], include_dirs=include_dirs),
-                  ]
-    if platform.system() == 'Windows':
-        extensions += [Extension("storm_analysis.L1H._fista_lib", ["./storm_analysis/L1H/fista_lib.c"],
-                                 libraries=library_dirs, include_dirs=include_dirs),
- 
-        ]
-
-    return extensions
+  def run(self):
+    self.run_command('build_c')
+    setuptools.command.build_py.build_py.run(self)
+    
 
 setup(
     name='storm_analysis',
@@ -123,10 +66,14 @@ setup(
     author_email='hbabcock at fas.harvard.edu',
     url='https://github.com/ZhuangLab/storm-analysis',
 
+    cmdclass={
+        'build_c' : SConsCommand,
+        'build_py' : BuildPyCommand,
+    },
+
     zip_safe=False,
     packages=find_packages(),
 
-    ext_modules=get_c_extensions(),
     package_data={},
     exclude_package_data={},
     include_package_data=True,
