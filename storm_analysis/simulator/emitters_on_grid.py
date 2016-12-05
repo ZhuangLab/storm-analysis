b5fd6d269a2efd299c2f19c150d25031cb102273
Hazen Babcock <hbabcock@mac.com>
2016-12-05 12:00:29 -0500

    Use argparse module for command line arguments.

--- storm_analysis/simulator/emitters_on_grid.py
+++ storm_analysis/simulator/emitters_on_grid.py
@@ -5,22 +5,30 @@
 # Hazen 12/16
 #
 
+import argparse
 import random
-import sys
 
 import storm_analysis.sa_library.i3dtype as i3dtype
 import storm_analysis.sa_library.writeinsight3 as writeinsight3
 
+parser = argparse.ArgumentParser(description = "Create a grid of emitters for simulations.")
 
-if (len(sys.argv) != 5):
-    print("usage: <bin_file> <nx> <ny> <spacing>")
-    exit()
+parser.add_argument('--bin', dest='i3bin', type=str, required=True,
+                    help = "The name of Insight3 format file to save the emitter locations, etc.")
+parser.add_argument('--nx', dest='nx', type=int, required=True,
+                    help = "The grid size in X.")
+parser.add_argument('--ny', dest='ny', type=int, required=True,
+                    help = "The grid size in Y.")
+parser.add_argument('--spacing', dest='spacing', type=float, required=True,
+                    help = "The grid spacing in pixels.")
+
+args = parser.parse_args()
 
 random.seed(0)
 
-nx = int(sys.argv[2])
-ny = int(sys.argv[3])
-spacing = float(sys.argv[4])
+nx = args.nx
+ny = args.ny
+spacing = args.spacing
 
 i3data = i3dtype.createDefaultI3Data(nx * ny)
 
@@ -33,5 +41,5 @@ for i in range(nx):
         cury += spacing
     curx += spacing
 
-with writeinsight3.I3Writer(sys.argv[1]) as i3w:
+with writeinsight3.I3Writer(args.i3bin) as i3w:
     i3w.addMolecules(i3data)
