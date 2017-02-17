44b358559c7a2de8a3825884a8a4fd3736b5bfd8
Hazen Babcock <hbabcock@fas.harvard.edu>
2017-02-17 16:44:55 -0500

    Add verification of the drift correction results.

--- storm_analysis/test/test_rcc.py
+++ storm_analysis/test/test_rcc.py
@@ -2,15 +2,29 @@
 
 import storm_analysis
 
+import storm_analysis.test.verify_drift_correction as vDC
 
 def test_rcc():
 
+    # Calculate drift correction.
     mlist_name = storm_analysis.getData("test/data/test_drift_mlist.bin")
-    drift_name = storm_analysis.getPathOutputTest("test_drift.txt")
+    drift_name = storm_analysis.getPathOutputTest("test_rcc_drift.txt")
 
     from storm_analysis.rcc.rcc_drift_correction import rccDriftCorrection
-    rccDriftCorrection(mlist_name, drift_name, 2000, 1, True, False)
+    rccDriftCorrection(mlist_name, drift_name, 1000, 1, True, False)
 
+    # Verify results.
+    diffs = vDC.verifyDriftCorrection(storm_analysis.getData("test/data/test_drift.txt"), drift_name)
+    
+    if (diffs[0] > 0.1):
+        raise Exception("Frame numbers do not match.")
+
+    # These thresholds are somewhat arbitrary.
+    if (diffs[1] > 0.1) or (diffs[2] > 0.1):
+        raise Exception("XY drift correction error.")
+
+    if (diffs[3] > 30.0):
+        raise Exception("Z drift correction error.")    
 
 if (__name__ == "__main__"):
     test_rcc()
