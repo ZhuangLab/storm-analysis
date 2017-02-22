1e8e27e8b48ae3bb02310fb88daf3c25aadf0dc6
Hazen Babcock <hbabcock@fas.harvard.edu>
2017-02-22 10:53:24 -0500

    Initial commit.

---
+++ storm_analysis/test/__init__.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+import os
+
+
+# Maybe there is a builtin function that does this??
+def asciiString(value):
+    return str(value).encode("ascii")
+
+def getData(data_path):
+    import pkg_resources
+    data = pkg_resources.resource_filename(__name__, data_path)
+    return data
+
+
+def getPath(path):
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), path)
+
+
+def getPathOutputTest(fname=None):
+    out_path = getPath("test/output/")
+    
+    if not os.path.exists(out_path):
+        os.makedirs(out_path)
+    
+    if fname:
+        return os.path.join(out_path, fname)
+    else:
+        return out_path
+
+
+def removeFile(fname):
+    try:
+        os.remove(fname)
+    except OSError:
+        pass
+
+    
