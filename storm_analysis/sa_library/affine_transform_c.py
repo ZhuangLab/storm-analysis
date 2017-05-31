d7cb8b85388a353b7fd9d8e0a27ddb577192d69d
Hazen Babcock <hbabcock@mac.com>
2017-05-31 10:53:45 -0400

    Fix date.

--- storm_analysis/sa_library/affine_transform_c.py
+++ storm_analysis/sa_library/affine_transform_c.py
@@ -81,7 +81,7 @@ if (__name__ == "__main__"):
 #
 # The MIT License
 #
-# Copyright (c) 2016 Babcock Lab, Harvard University
+# Copyright (c) 2017 Babcock Lab, Harvard University
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
