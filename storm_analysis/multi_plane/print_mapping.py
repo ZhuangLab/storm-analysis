0278e985b4b36756593f4d87b822afe801db3597
Hazen Babcock <hbabcock@mac.com>
2017-06-15 15:15:46 -0400

    Tweak comment. Move module functionality into a function.

--- storm_analysis/multi_plane/print_mapping.py
+++ storm_analysis/multi_plane/print_mapping.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python
 """
-Print out a mapping file. A debugging tool.
+Print out a mapping file. Primarily a debugging tool.
 
 Hazen 05/17
 """
 
-import argparse
 import pickle
 
-parser = argparse.ArgumentParser(description = 'Print out a mapping file.')
-
-parser.add_argument('--mapping', dest='mapping', type=str, required=True,
-                    help = "The name of the mapping file.")
-
-args = parser.parse_args()
-
-with open(args.mapping, 'rb') as fp:
-    mappings = pickle.load(fp)
+def printMapping(filename):
+    with open(filename, 'rb') as fp:
+        mappings = pickle.load(fp)
     
-for key in sorted(mappings):
-    print(key, mappings[key])
+    for key in sorted(mappings):
+        print(key, mappings[key])
+
+
+if (__name__ == "__main__"):
+    import argparse
+    parser = argparse.ArgumentParser(description = 'Print out a mapping file.')
+
+    parser.add_argument('--mapping', dest='mapping', type=str, required=True,
+                        help = "The name of the mapping file.")
+
+    args = parser.parse_args()
+
+    printMapping(args.mapping)
