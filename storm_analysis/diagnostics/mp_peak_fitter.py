74ff84ce82b606107db66b5dec3eca36b03f1081
Hazen Babcock <hbabcock@mac.com>
2017-06-27 23:40:05 -0400

    Add analyze() function and command line arguments. Fix a few bugs.

--- storm_analysis/diagnostics/mp_peak_fitter.py
+++ storm_analysis/diagnostics/mp_peak_fitter.py
@@ -20,21 +20,19 @@ import storm_analysis.sa_utilities.std_analysis as stdAnalysis
 import storm_analysis.multi_plane.find_peaks_std as findPeaksStd
 
 
-xy_error = None
-z_error = None
-
 class PFMovieReader(findPeaksStd.MPMovieReader):
 
     def __init__(self, bin_name = None, **kwds):
         super().__init__(**kwds)
 
         self.i3_data = readinsight3.loadI3File(bin_name)
+        self.nm_per_pixel = self.parameters.getAttr("pixel_size")
 
     def getLocalizations(self):
         
         #
-        # Return groud truth localization positions in the multi-fit
-        # peak format.
+        # Return ground truth localization positions in the
+        # multi-fit peak format.
         #
         mf_peaks = i3dtype.convertToMultiFit(self.i3_data,
                                              self.movie_x,
@@ -60,7 +58,7 @@ class PFPeakFinder(findPeaksStd.MPPeakFinder):
         return self.mpu.splitPeaks(peaks)
     
     
-class PFPeakFinderFitter(findPeakStd.MPPeakFinderFitter):
+class PFPeakFinderFitter(findPeaksStd.MPFinderFitter):
 
    def analyzeImage(self, movie_reader, save_residual = False, verbose = False):
 
@@ -71,7 +69,7 @@ class PFPeakFinderFitter(findPeakStd.MPPeakFinderFitter):
        self.peak_fitter.newImages(images)
 
        # Load ground truth localization positions.
-       truth_peaks = movieReader.getLocalizations()
+       truth_peaks = movie_reader.getLocalizations()
 
        # Adjust to work with multi-plane.
        fit_peaks = self.peak_finder.findPeaks(truth_peaks.copy())
@@ -85,3 +83,36 @@ class PFPeakFinderFitter(findPeakStd.MPPeakFinderFitter):
        
        return [fit_peaks, None]
         
+
+def analyze(base_name, mlist_name, olist_name, settings_name):
+    parameters = params.ParametersMultiplane().initFromFile(settings_name)
+    finder = PFPeakFinder(parameters)
+    fitter = findPeaksStd.MPPeakFitter(parameters)
+    finder_fitter = PFPeakFinderFitter(parameters, finder, fitter)
+    reader = PFMovieReader(base_name = base_name,
+                           bin_name = olist_name,
+                           parameters = parameters)
+    stdAnalysis.standardAnalysis(finder_fitter,
+                                 reader,
+                                 mlist_name,
+                                 parameters)
+
+
+if (__name__ == "__main__"):
+
+    import argparse
+
+    parser = argparse.ArgumentParser(description = 'Multi-plane peak fitter diagnostics - ...')
+
+    parser.add_argument('--basename', dest='basename', type=str, required=True,
+                        help = "The base name of the movie to analyze. Movies can be in .dax, .tiff or .spe format.")
+    parser.add_argument('--bin', dest='mlist', type=str, required=True,
+                        help = "The name of the localizations output file. This is a binary file in Insight3 format.")
+    parser.add_argument('--truth', dest='olist', type=str, required=True,
+                        help = "The name of the truth localizations file. This is a binary file in Insight3 format.")
+    parser.add_argument('--xml', dest='settings', type=str, required=True,
+                        help = "The name of the settings xml file.")
+
+    args = parser.parse_args()
+    
+    analyze(args.basename, args.mlist, args.olist, args.settings)
