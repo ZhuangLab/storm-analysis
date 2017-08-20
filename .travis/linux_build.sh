1c45f97bd9b3b1d661aa872d3658a202fc680690
Hazen Babcock <hbabcock@mac.com>
2017-08-20 14:52:24 -0400

    Try to fix issues with if statement.

--- .travis/linux_build.sh
+++ .travis/linux_build.sh
@@ -10,9 +10,10 @@ sudo apt update -qq
 sudo apt-get --yes install liblapack-dev
 sudo apt-get --yes install libfftw3-dev
 
+echo "$TOXENV"
+
 # Download Python2 or Python3 Miniconda.
-if [[ $TOXENV == "2.7" ]]
-then
+if [[ $TOXENV == "2.7" ]]; then
     wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh
 else
     wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
@@ -36,8 +37,7 @@ conda info -a
 conda create -q -n test-environment python=$TOXENV gcc
 
 # Need Python2 for SCons.
-if [[ $TOXENV == "2.7" ]];
-then
+if [[ $TOXENV == '2.7' ]]; then
     scons
 else
     conda create -q -n py2-env python=2.7
@@ -46,16 +46,17 @@ else
 fi
 
 # Activate virtual environment test-environment.
-source activate test-environment
+#source activate test-environment
 
 # Install conda dependencies.
-conda config --add channels conda-forge
-conda install numpy scipy pytest matplotlib
-conda install tifffile pillow
-conda install shapely randomcolor pywavelets
+#conda config --add channels conda-forge
+#conda install numpy scipy pytest matplotlib
+#conda install tifffile pillow
+#conda install shapely randomcolor pywavelets
 
-# Install nose2 testing package.
-pip install nose2
+# Install packages for running test.
+#   I think we need these but 
+#conda install pytest pytest-runner
 
 # Install the storm-analysis project.
-python setup.py install
+#python setup.py install
