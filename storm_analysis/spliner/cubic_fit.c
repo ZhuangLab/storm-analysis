50bbfc7b409bf305f67d26c2e4e103147f46b7e7
Hazen Babcock <hbabcock@fas.harvard.edu>
2017-10-06 19:50:50 -0400

    Remove dposv_() function definition.

--- storm_analysis/spliner/cubic_fit.c
+++ storm_analysis/spliner/cubic_fit.c
@@ -12,10 +12,6 @@
 
 #include "cubic_fit.h"
 
-/* LAPACK Functions */
-extern void dposv_(char* uplo, int* n, int* nrhs, double* a, int* lda,
-		   double* b, int* ldb, int* info);
-
 
 /*
  * cfAddPeak()
