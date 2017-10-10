247bdcefc1614bcd351e21b659dbcc2df5e23f1b
Hazen Babcock <hbabcock@fas.harvard.edu>
2017-10-10 13:40:46 -0400

    Add test for XYZ translation.

--- storm_analysis/test/test_pupilfn.py
+++ storm_analysis/test/test_pupilfn.py
@@ -9,6 +9,10 @@ import storm_analysis.pupilfn.pupil_function_c as pfFnC
 import tifffile
 
 def test_pupilfn_1():
+    """
+    Test that the C and the Python library agree on the calculation
+    of the untranslated PSF.
+    """
     geo = pupilMath.Geometry(20, 0.1, 0.6, 1.5, 1.4)
     pf = geo.createFromZernike(1.0, [[1.3, 2, 2]])
 
@@ -18,13 +22,46 @@ def test_pupilfn_1():
     psf_c = pf_c.getPSF()
     psf_py = pupilMath.intensity(pupilMath.toRealSpace(pf))
 
-    with tifffile.TiffWriter(storm_analysis.getPathOutputTest("test_pupilfn_1.tif")) as tf:
-        tf.save(psf_c.astype(numpy.float32))
-        tf.save(psf_py.astype(numpy.float32))
+    assert (numpy.max(numpy.abs(psf_c - psf_py))) < 1.0e-10
+
+    if False:
+        with tifffile.TiffWriter(storm_analysis.getPathOutputTest("test_pupilfn_1.tif")) as tf:
+            tf.save(psf_c.astype(numpy.float32))
+            tf.save(psf_py.astype(numpy.float32))
+
+    pf_c.cleanup()
+
+def test_pupilfn_2():
+    """
+    Test PF translation.
+    """
+    dx = 0.5
+    dy = 0.25
+    dz = 0.2
+    geo = pupilMath.Geometry(20, 0.1, 0.6, 1.5, 1.4)
+    pf = geo.createFromZernike(1.0, [[1.3, 2, 2]])
+
+    pf_c = pfFnC.PupilFunction(geometry = geo)
+    pf_c.setPF(pf)
+
+    pf_c.translate(dx, dy, -dz)
+    psf_c = pf_c.getPSF()
+
+    defocused = geo.changeFocus(pf, dz)
+    translated = geo.translatePf(defocused, dx, dy)
+    psf_py = pupilMath.intensity(pupilMath.toRealSpace(translated))
+
+    assert (numpy.max(numpy.abs(psf_c - psf_py))) < 1.0e-10
+
+    if False:
+        with tifffile.TiffWriter(storm_analysis.getPathOutputTest("test_pupilfn_2.tif")) as tf:
+            tf.save(psf_c.astype(numpy.float32))
+            tf.save(psf_py.astype(numpy.float32))
 
     pf_c.cleanup()
 
 
-
 if (__name__ == "__main__"):
     test_pupilfn_1()
+    test_pupilfn_2()
+
