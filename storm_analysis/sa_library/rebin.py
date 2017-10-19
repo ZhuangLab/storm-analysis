db646cbb1845c26c6321e1e5d1ba0adde88c7477
Hazen Babcock <hbabcock@fas.harvard.edu>
2017-10-19 16:03:21 -0400

    Add downsampling function.

--- storm_analysis/sa_library/rebin.py
+++ storm_analysis/sa_library/rebin.py
@@ -9,6 +9,32 @@ import numpy
 import scipy
 import scipy.fftpack
 
+#
+# This comes directly from here:
+#
+# http://scipy-cookbook.readthedocs.io/items/Rebinning.html
+#
+# Copyright 2015, Various authors. Revision 28f8936a.
+# 
+def downSample(a, *args, verbose = False):
+    '''rebin ndarray data into a smaller ndarray of the same rank whose dimensions
+    are factors of the original dimensions. eg. An array with 6 columns and 4 rows
+    can be reduced to have 6,3,2 or 1 columns and 4,2 or 1 rows.
+    example usages:
+    >>> a=rand(6,4); b=rebin(a,3,2)
+    >>> a=rand(6); b=rebin(a,2)
+    '''
+    shape = a.shape
+    lenShape = len(shape)
+    factor = numpy.asarray(shape)/numpy.asarray(args)
+    evList = ['a.reshape('] + \
+             ['args[%d],int(factor[%d]),'%(i,i) for i in range(lenShape)] + \
+             [')'] + ['.sum(%d)'%(i+1) for i in range(lenShape)] + \
+             ['/factor[%d]'%i for i in range(lenShape)]
+    if verbose:
+        print(''.join(evList))
+    return eval(''.join(evList))
+
 def upSampleFFT(image, factor):
     """
     Upsample using a FFT (high frequencies are set to zero).
