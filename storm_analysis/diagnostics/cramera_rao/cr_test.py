1ad9e49c86e1dca8e707d9c6d4da2800173fe734
Hazen Babcock <hbabcock@mac.com>
2017-10-24 17:34:34 -0400

    Initial commit.

---
+++ storm_analysis/diagnostics/cramera_rao/cr_test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+"""
+Test(s) of Cramer-Rao bounds calculations.
+
+Hazen 10/17
+"""
+
+import storm_analysis.psf_fft.cramer_rao as psfFFTCramerRao
+import storm_analysis.pupilfn.cramer_rao as pupilFnCramerRao
+import storm_analysis.spliner.cramer_rao as splinerCramerRao
+
+import settings
+
+
+cr_psf_fft = psfFFTCramerRao.CRPSFFn(psf_filename = "psf_fft.psf",
+                                     pixel_size = settings.pixel_size)
+
+cr_pupil_fn = pupilFnCramerRao.CRPupilFn(psf_filename = "pupilfn.pfn",
+                                         pixel_size = settings.pixel_size,
+                                         zmax = settings.spline_z_range,
+                                         zmin = -settings.spline_z_range)
+
+cr_spline = splinerCramerRao.CRSplineToPSF3D(psf_filename = "psf.spline",
+                                             pixel_size = settings.pixel_size)
+
+cr_objects = [cr_psf_fft, cr_pupil_fn, cr_spline]
+for z in [-100.0, 0.0, 100.0]:
+    print(z)
+    for cro in cr_objects:
+        crbs = splinerCramerRao.calcCRBound3D(cro, 10, 1000, z)
+        print("{0:.1f} {1:.2f} {2:.2f} {3:.2f} {4:.4f}".format(*crbs))
+    print()
+        
---
+++ storm_analysis/diagnostics/cramera_rao/settings.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""
+Settings to use in Cramer-Rao calculations.
+
+Hazen 10/17
+"""
+pixel_size = 100.0
+psf_fft_z_range = 600.0
+psf_fft_z_step = 100.0
+spline_size = 20
+spline_z_range = 750.0
+x_size = 300
+y_size = 200
+zmn = [[1.3, 2, 2]]
+#zmn = [[1.3, -1, 3], [1.3, -2, 2]]
