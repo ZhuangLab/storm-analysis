af4a9d9d44b68998c2ea08625105177b78537691
Hazen Babcock <hbabcock@mac.com>
2017-11-08 15:36:14 -0500

    Add report of the total running time.

--- storm_analysis/diagnostics/daostorm_3d/analyze_data.py
+++ storm_analysis/diagnostics/daostorm_3d/analyze_data.py
@@ -11,7 +11,7 @@ import time
 import storm_analysis.daostorm_3d.mufit_analysis as dao3d
 
 dirs = sorted(glob.glob("test*"))
-
+total_time = 0.0
 for a_dir in dirs:
     print()
     print("Analyzing:", a_dir)
@@ -29,8 +29,11 @@ for a_dir in dirs:
     stop_time = time.time()
 
     # Save timing results.
-    print("Analysis completed in {0:.2f} seconds".format(stop_time - start_time))
+    total_time += stop_time - start_time
+    print("Analysis completed in {0:.2f} seconds.".format(stop_time - start_time))
 
     with open(a_dir + "/timing.txt", "w") as fp:
         fp.write(str(stop_time - start_time) + "\n")
 
+print()
+print("{0:d} directories analyzed in {1:.2f} seconds.".format(len(dirs), total_time))
