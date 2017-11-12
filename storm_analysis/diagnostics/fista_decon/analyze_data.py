0f563a6181c1798f54792e5645c08fff4e379d82
Hazen Babcock <hbabcock@mac.com>
2017-11-12 18:37:59 -0500

    Finish initial version of FISTA decon testing.

--- storm_analysis/diagnostics/fista_decon/analyze_data.py
+++ storm_analysis/diagnostics/fista_decon/analyze_data.py
@@ -12,6 +12,7 @@ import storm_analysis.spliner.spline_analysis as sp_ana
 
 dirs = sorted(glob.glob("test*"))
 
+total_time = 0.0
 for a_dir in dirs:
     print()
     print("Analyzing:", a_dir)
@@ -29,8 +30,11 @@ for a_dir in dirs:
     stop_time = time.time()
 
     # Save timing results.
+    total_time += stop_time - start_time
     print("Analysis completed in {0:.2f} seconds".format(stop_time - start_time))
 
     with open(a_dir + "/timing.txt", "w") as fp:
         fp.write(str(stop_time - start_time) + "\n")
 
+print()
+print("{0:d} directories analyzed in {1:.2f} seconds.".format(len(dirs), total_time))
