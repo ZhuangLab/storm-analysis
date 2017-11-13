Fix logic error in peakToPeakDistAndIndex(), basically the query was being done in the wrong order.

storm_analysis/sa_library/ia_utilities_c.py
@@ -273,11 +273,11 @@ def peakToPeakDistAndIndex(x1, y1, x2, y2):
            the C version, figuring out where the cross over occurs, and using
            which ever is fastest depending on the peak list size.
     """
-    # Make kdtree from x1, y1.
-    kd = scipy.spatial.KDTree(numpy.stack((x1, y1), axis = 1))
+    # Make kdtree from x2, y2.
+    kd = scipy.spatial.KDTree(numpy.stack((x2, y2), axis = 1))
 
-    # Make point pairs of x2, y2.
-    pnts = numpy.stack((x2, y2), axis = 1)
+    # Make point pairs of x1, y1.
+    pnts = numpy.stack((x1, y1), axis = 1)
 
     return kd.query(pnts)
 