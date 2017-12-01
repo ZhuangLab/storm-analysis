8e6e62cf3430f9a1010da70dec9384825d0ead2b
Hazen Babcock <hbabcock@mac.com>
2017-12-01 09:21:35 -0500

    Pull updated version from smoother_error branch.

--- storm_analysis/diagnostics/daostorm_3d/make_data.py
+++ storm_analysis/diagnostics/daostorm_3d/make_data.py
@@ -36,6 +36,8 @@ index = 1
 # test_01	0.028	0.106	0.028	0.106
 # test_02	0.019	0.101	0.019	0.101
 #
+
+# Gaussian PSF.
 if True:
     for [bg, photons] in settings.photons:
 
@@ -59,6 +61,31 @@ if True:
         sim.simulate(wdir + "/test.dax", "grid_list.bin", settings.n_frames)
         
         index += 1
+
+# Pupil Function PSF.
+if False:
+    for [bg, photons] in settings.photons:
+
+        wdir = "test_{0:02d}".format(index)
+        print(wdir)
+        if not os.path.exists(wdir):
+            os.makedirs(wdir)
+
+        bg_f = lambda s, x, y, i3 : background.UniformBackground(s, x, y, i3, photons = bg)
+        cam_f = lambda s, x, y, i3 : camera.Ideal(s, x, y, i3, settings.camera_offset)
+        pp_f = lambda s, x, y, i3 : photophysics.AlwaysOn(s, x, y, i3, photons)
+        psf_f = lambda s, x, y, i3 : psf.PupilFunction(s, x, y, i3, settings.pixel_size, [])
+
+        sim = simulate.Simulate(background_factory = bg_f,
+                                camera_factory = cam_f,
+                                photophysics_factory = pp_f,
+                                psf_factory = psf_f,
+                                x_size = settings.x_size,
+                                y_size = settings.y_size)
+    
+        sim.simulate(wdir + "/test.dax", "grid_list.bin", settings.n_frames)
+        
+        index += 1        
 
 # Non-uniform background, always on.
 if False:
