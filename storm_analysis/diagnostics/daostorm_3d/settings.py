ba8a6c02ee5b868afa3bca0de675edfada63b414
Hazen Babcock <hbabcock@mac.com>
2017-12-14 09:16:43 -0500

    Some tweaks to the settings.

--- storm_analysis/diagnostics/daostorm_3d/settings.py
+++ storm_analysis/diagnostics/daostorm_3d/settings.py
@@ -7,13 +7,13 @@ Hazen 09/17
 
 camera_gain = 1.0
 camera_offset = 100.0
-iterations = 20
-model = "2d"
+iterations = 1
+model = "2dfixed"
 n_frames = 100
 nx = 14
 ny = 9
-photons = [[20, 500], [20, 1000]]
-#photons = [[100, 1000]]
+#photons = [[20, 500], [20, 1000]]
+photons = [[50, 2000]]
 pixel_size = 100.0
 tolerance = 0.3
 x_size = 300
--- storm_analysis/diagnostics/sCMOS/settings.py
+++ storm_analysis/diagnostics/sCMOS/settings.py
@@ -7,15 +7,15 @@ Hazen 09/17
 
 camera_gain = 1.0
 camera_offset = 100.0
-camera_variance = 2.0
+camera_variance = 2.5
 iterations = 20
-model = "2d"
+model = "2dfixed"
 n_frames = 100
 nx = 14
 ny = 9
 photons = [[20, 500], [20, 1000]]
 pixel_size = 100.0
-random_variance = True
+random_variance = False
 tolerance = 0.3
 x_size = 300
 y_size = 200
