0d175711b8deb7c304069caf010babd768bba76a
Hazen Babcock <hbabcock@mac.com>
2017-12-19 21:58:17 -0500

    Add storing of movie size, saving and loading localizations. Add additional tests.

--- storm_analysis/sa_library/sa_h5py.py
+++ storm_analysis/sa_library/sa_h5py.py
@@ -21,8 +21,12 @@ class SAH5Py(object):
    
     The metadata is stored in the 'metadata.xml' root dataset as a 
     variable length unicode string.
+
+    The 'sa_type' attribute is either 'analysis' or 'merged'. For 
+    'merged' files frame number no longer has any meaning, it just 
+    represents the order in which the data was added to the file.
     """
-    def __init__(self, filename = None, **kwds):
+    def __init__(self, filename = None, sa_type = 'analysis', **kwds):
         super(SAH5Py, self).__init__(**kwds)
 
         if os.path.exists(filename):
@@ -30,6 +34,7 @@ class SAH5Py(object):
         else:
             self.hdf5 = h5py.File(filename, "w")
             self.hdf5.attrs['version'] = 0.1
+            self.hdf5.attrs['sa_type'] = sa_type
             
     def __enter__(self):
         return self
@@ -38,9 +43,31 @@ class SAH5Py(object):
         if self.hdf5:
             self.hdf5.close()
 
-    def addLocalizations(self, localizations, frame_number):
-        pass
-    
+    def addLocalizations(self, localizations, frame_number, channel = None):
+        """
+        Add localization data to the HDF5 file. Each element of localizations
+        is stored in it's own dataset. In the case of multi-channel data, the
+        data from the other channels is stored in a sub-groups of the frame
+        group. Both 'frame_number' and 'channel' should be integers.
+        """
+        assert(isinstance(frame_number, int))
+        
+        grp_name = "fr_" + str(frame_number)
+        if channel is None:
+            grp = self.hdf5.create_group("fr_" + str(frame_number))
+
+            # Add initial values for drift correction. These only apply to
+            # channel 0.
+            grp.attrs['dx'] = 0.0
+            grp.attrs['dy'] = 0.0
+            grp.attrs['dz'] = 0.0
+        else:
+            assert(isinstance(channel, int))
+            grp = self.hdf5[grp_name].create_group("ch_" + str(channel))
+
+        for key in localizations:
+            grp.create_dataset(key, data = localizations[key])
+            
     def addMetadata(self, metadata):
         """
         Add metadata to the HDF5 file, this is the contents of the XML file
@@ -48,7 +75,6 @@ class SAH5Py(object):
         the size of the movie.
         """
         assert(isinstance(metadata, str))
-        assert(not "metadata.xml" in self.hdf5)
 
         #dt = h5py.special_dtype(vlen = unicode)
         dt = h5py.special_dtype(vlen = str)
@@ -59,12 +85,79 @@ class SAH5Py(object):
         dset[:len(metadata)] = metadata
         self.hdf5.flush()
 
+    def addMovieInformation(self, movie_reader):
+        """
+        Store some properties of the movie as attributes.
+        """
+        self.hdf5.attrs['movie_hash_value'] = movie_reader.hashID()
+        self.hdf5.attrs['movie_l'] = movie_reader.getMovieL()
+        self.hdf5.attrs['movie_x'] = movie_reader.getMovieX()
+        self.hdf5.attrs['movie_y'] = movie_reader.getMovieY()
+        
     def close(self):
         self.hdf5.close()
 
+    def getFileType(self):
+        return self.hdf5.attrs['sa_type']
+    
     def getFileVersion(self):
         return self.hdf5.attrs['version']
+
+    def getLocalizations(self, channel = None, fields = None):
+        return self.getLocalizationsInFrameRange(0,
+                                                 self.hdf5.attrs['movie_l'],
+                                                 channel = channel,
+                                                 fields = fields)
+        
+    def getLocalizationsInFrame(self, frame_number, channel = None, fields = None):
+        assert(isinstance(frame_number, int))
+
+        locs = {}
+        grp_name = "/fr_" + str(frame_number)
+        if channel is not None:
+            grp_name += "/ch_" + str(channel)
+
+        if grp_name in self.hdf5:
+            grp = self.hdf5[grp_name]
+
+            if fields is None:
+                for field in grp:
+                    if not (field[:3] == "ch_"):
+                        locs[field] = grp[field][()]
+
+            else:
+                for field in fields:
+                    locs[field] = grp[field][()]
+
+        return locs
+
+    def getLocalizationsInFrameRange(self, start, stop, channel = None, fields = None):
+        """
+        Return the localizations in the range start <= frame number < stop.
+        """
+        assert(stop > start)
+        locs = {}
+        for i in range(start, stop):
+            temp = self.getLocalizationsInFrame(i, channel = channel, fields = fields)
+            if(not bool(temp)):
+                continue
+            
+            for field in temp:
+                if field in locs:
+                    locs[field] = numpy.concatenate((locs[field], temp[field]))
+                else:
+                    locs[field] = temp[field]
+
+        return locs
     
     def getMetadata(self):
-        return self.hdf5["metadata.xml"][0]
+        if "metadata.xml" in self.hdf5:
+            return self.hdf5["metadata.xml"][0]
+    
+    def getMovieInformation(self):
+        return [self.hdf5.attrs['movie_x'],
+                self.hdf5.attrs['movie_y'],
+                self.hdf5.attrs['movie_l'],
+                self.hdf5.attrs['movie_hash_value']]
+
     
--- storm_analysis/test/test_sa_h5py.py
+++ storm_analysis/test/test_sa_h5py.py
@@ -3,11 +3,33 @@
 Tests of our HDF5 reader, or perhaps more accurately test of our 
 understanding of how to use the h5py module.
 """
+import numpy
 import os
 import storm_analysis
 
 import storm_analysis.sa_library.sa_h5py as saH5py
 
+
+class FakeReader(object):
+    """
+    Fake movie reader object.
+    """
+    def __init__(self, **kwds):
+        super(FakeReader, self).__init__(**kwds)
+
+    def hashID(self):
+        return "xyzzy"
+
+    def getMovieL(self):
+        return 10
+
+    def getMovieX(self):
+        return 128
+
+    def getMovieY(self):
+        return 128
+
+    
 def test_sa_h5py_1():
     """
     Test metadata round trip.
@@ -24,11 +46,81 @@ def test_sa_h5py_1():
 
     # Read metadata.
     with saH5py.SAH5Py(h5_name) as h5:
-        print(h5.getMetadata(), type(h5.getMetadata()))
+        assert(metadata == h5.getMetadata())
 
-    
+
+def test_sa_h5py_2():
+    """
+    Test data round trip.
+    """
+    peaks = {"x" : numpy.zeros(10),
+             "y" : numpy.ones(10)}
+
+    filename = "test_sa_hdf5.hdf5"
+    h5_name = storm_analysis.getPathOutputTest(filename)
+    storm_analysis.removeFile(h5_name)
+
+    # Write data.
+    with saH5py.SAH5Py(h5_name) as h5:
+        h5.addLocalizations(peaks, 1)
+        h5.addLocalizations(peaks, 1, channel = 1)
+
+    # Read data.
+    with saH5py.SAH5Py(h5_name) as h5:
+
+        # Check that frame 0 is empty.
+        locs = h5.getLocalizationsInFrame(0)
+        assert(not bool(locs))
+
+        # Check frame1.
+        locs = h5.getLocalizationsInFrame(1)
+        assert(numpy.allclose(peaks["x"], locs["x"]))
+        assert(numpy.allclose(peaks["y"], locs["y"]))
+
+        # Check frame1, channel 0.
+        locs = h5.getLocalizationsInFrame(1, channel = 1)
+        assert(numpy.allclose(peaks["x"], locs["x"]))
+        assert(numpy.allclose(peaks["y"], locs["y"]))
+
+        # Check getting a specific field.
+        locs = h5.getLocalizationsInFrame(1, fields = ["x"])
+        assert("x" in locs)
+        assert(not "y" in locs)
+
+
+def test_sa_h5py_3():
+    """
+    Test getting data from multiple frames.
+    """
+    peaks = {"x" : numpy.zeros(10),
+             "y" : numpy.ones(10)}
+
+    filename = "test_sa_hdf5.hdf5"
+    h5_name = storm_analysis.getPathOutputTest(filename)
+    storm_analysis.removeFile(h5_name)
+
+    # Write data.
+    fr = FakeReader()
+    with saH5py.SAH5Py(h5_name) as h5:
+        h5.addMovieInformation(fr)
+        for i in range(fr.getMovieL()):
+            h5.addLocalizations(peaks, i)
+
+    # Read data.
+    with saH5py.SAH5Py(h5_name) as h5:
+
+        # Check localizations in first 5 frames.
+        locs = h5.getLocalizationsInFrameRange(0,5)
+        assert(locs["x"].size == 50)
+
+        # Get all the localizations.
+        locs = h5.getLocalizations()
+        assert(locs["x"].size == (10.0 * fr.getMovieL()))
+        
+
 if (__name__ == "__main__"):
-    test_sa_h5py_1()
-    
+#    test_sa_h5py_1()
+#    test_sa_h5py_2()
+    test_sa_h5py_3()
 
     
