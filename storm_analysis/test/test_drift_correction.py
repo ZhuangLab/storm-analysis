aa09d982ac071ca2c51123e0d0016a55a3aed309
Hazen Babcock <hbabcock@mac.com>
2017-12-25 16:45:44 -0500

    Add test of interpolation.

--- storm_analysis/test/test_drift_correction.py
+++ storm_analysis/test/test_drift_correction.py
@@ -3,18 +3,21 @@
 import numpy
 
 import storm_analysis
+import storm_analysis.sa_library.drift_utilities as driftUtils
 import storm_analysis.sa_library.parameters as params
 import storm_analysis.sa_utilities.xyz_drift_correction as xyzDriftCorrection
 
 import storm_analysis.test.verifications as veri
 
-def test_drift_correction():
-
+def test_drift_correction_1():
+    """
+    This tests the whole process.
+    """
     # Calculate drift correction.
     param_name = storm_analysis.getData("test/data/test_drift.xml")    
     parameters = params.ParametersCommon().initFromFile(param_name)
 
-    mlist_name = storm_analysis.getData("test/data/test_drift_mlist.bin")
+    mlist_name = storm_analysis.getData("test/data/test_drift.hdf5")
     drift_output = storm_analysis.getPathOutputTest("test_drift_drift.txt")
 
     [min_z, max_z] = parameters.getZRange()
@@ -22,8 +25,8 @@ def test_drift_correction():
                                           drift_output,
                                           parameters.getAttr("frame_step"),
                                           parameters.getAttr("d_scale"),
-                                          1000.0 * min_z,
-                                          1000.0 * max_z,
+                                          min_z,
+                                          max_z,
                                           True)
 
     # Verify results.
@@ -39,7 +42,29 @@ def test_drift_correction():
     if (diffs[3] > 30.0):
         raise Exception("Z drift correction error.")
 
+def test_drift_correction_2():
+    """
+    Test interpolation.
+    """
+    x_vals = numpy.array([3,6,9,12])
+    y_vals = numpy.array([1,2,2,1])
+    int_y = driftUtils.interpolateData(x_vals, y_vals, 16)
 
+    exp_y = numpy.array([0.0, 1.0/3.0, 2.0/3.0, 1.0, 4.0/3.0, 5.0/3.0, 2.0, 2.0,
+                         2.0, 2.0, 5.0/3.0, 4.0/3.0, 1.0, 2.0/3.0, 1.0/3.0, 0.0])
+
+    assert(numpy.allclose(int_y, exp_y))
+
+    if False:
+        import matplotlib
+        import matplotlib.pyplot as pyplot
+
+        int_x = numpy.arange(16)
+        pyplot.plot(int_x, int_y)
+        pyplot.show()
+
+    
 if (__name__ == "__main__"):
-    test_drift_correction()
+    test_drift_correction_1()
+    test_drift_correction_2()
     
