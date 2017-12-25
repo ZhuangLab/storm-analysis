88889ade2693b929ed52a81da67def7bfb8b28f0
Hazen Babcock <hbabcock@mac.com>
2017-12-24 23:06:31 -0500

    Change to HDF5.

--- storm_analysis/test/generators/drift_test_data.py
+++ storm_analysis/test/generators/drift_test_data.py
@@ -2,13 +2,12 @@
 """
 Generate a test file for drift correction.
 
-Hazen 02/17
+Hazen 12/17
 """
-
 import numpy
+import os
 
-import storm_analysis.sa_library.i3dtype as i3dtype
-import storm_analysis.sa_library.writeinsight3 as writeinsight3
+import storm_analysis.sa_library.sa_h5py as saH5Py
 
 clusters = 100
 length = 20000
@@ -16,16 +15,21 @@ p_on = 0.1
 
 dx = 1.0/length   # pixels.
 dy = 2.0/length   # pixels.
-dz = 200.0/length # nanometers.
+dz = 0.2/length   # microns.
 
 numpy.random.seed(0)
 
 cx = numpy.random.uniform(low = 20.0, high = 236.0, size = clusters)
 cy = numpy.random.uniform(low = 20.0, high = 236.0, size = clusters)
-cz = numpy.random.uniform(low = -300.0, high = 300.0, size = clusters)
+cz = numpy.random.uniform(low = -0.3, high = 0.3, size = clusters)
 
+hdf5_name = "../data/test_drift.hdf5"
+if os.path.exists(hdf5_name):
+    os.remove(hdf5_name)
+    
 with open("../data/test_drift.txt", "w") as drift_fp:
-    with writeinsight3.I3Writer("../data/test_drift_mlist.bin") as i3_fp:
+    with saH5Py.SAH5Py(hdf5_name) as h5:
+        h5.setMovieProperties(256, 256, length, "XYZZY")
         for i in range(length):
         
             if((i % 500) == 0):
@@ -42,12 +46,7 @@ with open("../data/test_drift.txt", "w") as drift_fp:
 
             x = cx + drift_x + numpy.random.normal(scale = 0.5, size = clusters)
             y = cy + drift_y + numpy.random.normal(scale = 0.5, size = clusters)
-            z = cz + drift_z + numpy.random.normal(scale = 50.0, size = clusters)
-            
-            i3d = i3dtype.createDefaultI3Data(number_on)
-            i3dtype.posSet(i3d, "x", x[on])
-            i3dtype.posSet(i3d, "y", y[on])
-            i3dtype.posSet(i3d, "z", z[on])
-            i3dtype.setI3Field(i3d, "fr", i+1)
+            z = cz + drift_z + numpy.random.normal(scale = 0.05, size = clusters)
 
-            i3_fp.addMolecules(i3d)
+            peaks = {"x" : x[on], "y" : y[on], "z" : z[on]}
+            h5.addLocalizations(peaks, i)
