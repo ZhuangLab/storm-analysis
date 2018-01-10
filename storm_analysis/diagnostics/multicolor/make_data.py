cf10f4f2d11888a5b4908d299372ed698086f765
Hazen Babcock <hbabcock@mac.com>
2018-01-10 16:20:47 -0500

    Initial commit.

---
+++ storm_analysis/diagnostics/multicolor/make_data.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+"""
+Make data for testing Multiplane. 
+
+The default tests are pretty easy as they are just relatively bright
+localizations on a grid.
+
+Hazen 09/17
+"""
+import numpy
+import os
+import pickle
+
+import storm_analysis.sa_library.sa_h5py as saH5Py
+
+import storm_analysis.simulator.background as background
+import storm_analysis.simulator.camera as camera
+import storm_analysis.simulator.photophysics as photophysics
+import storm_analysis.simulator.psf as psf
+import storm_analysis.simulator.simulate as simulate
+
+import settings
+
+index = 1
+
+# Create HDF5 files for each plane.
+#
+for elt in ["grid_list.hdf5", "random_storm.hdf5"]:
+    locs = saH5Py.loadLocalizations(elt)
+    locs["color"] = numpy.random.randint(4, size = locs["x"].size)
+    zo = locs["z"].copy()
+        
+    locs["z"][:] = zo + 1.0e-3 * settings.z_planes[0]
+    saH5Py.saveLocalizations("sim_input_c1_" + elt, locs)
+    for i in range(1,4):
+        locs["x"] += settings.dx
+        locs["y"] += settings.dy
+        locs["z"][:] = zo + 1.0e-3 * settings.z_planes[i]
+        saH5Py.saveLocalizations("sim_input_c" + str(i+1) + "_" + elt, locs)
+
+if True:
+        
+    # Create a movie for each plane.
+    for [bg, photons] in settings.photons:
+
+        # Adjust photons by the number of planes.
+        photons = photons/float(len(settings.z_planes))
+
+        wdir = "test_{0:02d}".format(index)
+        print(wdir)
+        if not os.path.exists(wdir):
+            os.makedirs(wdir)
+
+        for i in range(4):
+            bg_f = lambda s, x, y, i3 : background.UniformBackground(s, x, y, i3, photons = bg)
+            cam_f = lambda s, x, y, i3 : camera.SCMOS(s, x, y, i3, "calib.npy")
+            pp_f = lambda s, x, y, i3 : photophysics.AlwaysOnMC(s, x, y, i3, color = i, photons = photons)
+            psf_f = lambda s, x, y, i3 : psf.PupilFunction(s, x, y, i3, settings.pixel_size, [])
+
+            sim = simulate.Simulate(background_factory = bg_f,
+                                    camera_factory = cam_f,
+                                    photophysics_factory = pp_f,
+                                    psf_factory = psf_f,
+                                    x_size = settings.x_size,
+                                    y_size = settings.y_size)
+
+            
+            sim.simulate(wdir + "/test_c" + str(i+1) + ".dax",
+                         "sim_input_c" + str(i+1) + "_grid_list.hdf5",
+                         settings.n_frames)
+        
+        index += 1
+
+
---
+++ storm_analysis/diagnostics/multicolor/settings.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+"""
+Settings to use in Multicolor simulations.
+
+Note: Background photons are per plane, total photons are divided across all the planes.
+
+Hazen 01/18
+"""
+import numpy
+
+camera_gain = 1.0
+camera_offset = 100.0
+camera_variance = 1.0
+
+dx = 2.0
+dy = 1.0
+
+independent_heights = 1
+iterations = 20
+
+margin = 1
+n_frames = 2
+nx = 14
+ny = 9
+
+photons = [[10, 4000]]
+pixel_size = 100.0
+
+psf_size = 20
+psf_z_range = 750.0
+
+test_z_offset = 0.0
+test_z_range = 100.0
+
+tolerance = 0.3
+x_size = 300
+y_size = 200
+
+z_planes = [100.0, -100.0, 100.0, -100.0]
+z_value = [-300.0, 0.0, 300.0]
+
+
