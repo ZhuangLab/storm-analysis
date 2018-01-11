19227ddf04f3e51e6c257ace509c06feb15dd24b
Hazen Babcock <hbabcock@mac.com>
2018-01-11 13:17:47 -0500

    Update to work with HDF5 format localization files.

--- storm_analysis/multi_plane/kmeans_classifier.py
+++ storm_analysis/multi_plane/kmeans_classifier.py
@@ -1,84 +1,66 @@
 #!/usr/bin/env python
 """
-Given a k-means codebook and a set of paired localization files 
-create a new localization file with the k-mean category in the
-'c' field and distance from the nearest cluster center in
-the 'i' field.
+Given a k-means codebook and a (tracked) localizations file, assign
+a color and a distance to the nearest cluster center. These fields
+are added to tracks and are available as 'km_color' and 'km_distance'.
+Tracks that are to far away from a cluster center are assigned a color
+value of 9.
 
-Hazen 09/17
+Hazen 01/18
 """
 import numpy
 import scipy
 import scipy.cluster
 
-import storm_analysis.sa_library.readinsight3 as readinsight3
-import storm_analysis.sa_library.writeinsight3 as writeinsight3
+import storm_analysis.sa_library.sa_h5py as saH5Py
 
-def KMeansClassifier(codebook, input_basename, output_name, extensions = [".bin", "_ch1.bin", "_ch2.bin", "_ch3.bin"], max_distance = 80):
+def KMeansClassifier(codebook, h5_filename, max_distance = 80):
     """
-    Note: 
-      1. The default is that there are 4 color channels / cameras.
-      2. The maximum distance is in percent, so '80' means that the 20%
-         of the localizations that most distant from a cluster center
-         will put in category 9.
+    Note: The maximum distance is in percent, so '80' means that the 20%
+          of the localizations that are most distant from a cluster center
+          will put in category 9.
     """
     n_channels = codebook.shape[1]
-    assert (n_channels == len(extensions)), "Codebook size does not match data."
+    with saH5Py.SAH5Py(h5_filename) as h5:
+        assert (n_channels == h5.getNChannels()), "Codebook size does not match data."
 
-    # Create a reader for each file.
-    i3_readers = []
-    for ext in extensions:
-        i3_name = input_basename + ext
-        print(i3_name)
-        i3_readers.append(readinsight3.I3Reader(i3_name))
+        # Create height names array.
+        height_names = ["height"]
+        for i in range(1,h5.getNChannels()):
+            height_names.append(h5.getChannelPrefix(i) + "height")
 
-    # Create writer for the results.
-    i3_out = writeinsight3.I3Writer(output_name)
+        # Iterate over tracks.
+        for i, tracks in enumerate(h5.tracksIterator(fields = height_names)):
 
-    # Read first block of the first channel data.
-    i3_data = [i3_readers[0].nextBlock()]
-    while (i3_data[0] is not False):
-        print("working..")
+            # Create features array.
+            features = None
+            for j, elt in enumerate(height_names):
 
-        # Read the data from the other channels.
-        for i in range(1,len(i3_readers)):
-            i3_data.append(i3_readers[i].nextBlock())
+                # Initialize features array if necessary.
+                if features is None:
+                    features = numpy.zeros((tracks[elt].size, h5.getNChannels()))
+                features[:,j] = tracks[elt]
 
-        # Load height data for each channel.
-        features = numpy.zeros((i3_data[0].size, n_channels))
-        for i in range(len(i3_readers)):
-            features[:,i] = i3_data[i]['h']
-
-        # Normalize by total height.
-        total = numpy.sum(features, axis = 1)
-        for i in range(features.shape[0]):
-            features[i,:] = features[i,:]/total[i]
+            # Normalize by total height.
+            total = numpy.sum(features, axis = 1)
+            for j in range(features.shape[0]):
+                features[j,:] = features[j,:]/total[j]
     
-        # Whiten the features as recommended by Scipy.
-        features = scipy.cluster.vq.whiten(features)
+            # Whiten the features as recommended by Scipy.
+            features = scipy.cluster.vq.whiten(features)
 
-        # Classify using codebook.
-        [category, distance] = scipy.cluster.vq.vq(features, codebook)
-        dist_max = numpy.percentile(distance, max_distance)
+            # Classify using codebook.
+            [km_color, km_distance] = scipy.cluster.vq.vq(features, codebook)
+            dist_max = numpy.percentile(km_distance, max_distance)
 
-        # Put top XX% in distance in category 9 (the discard category).
-        mask = (distance > dist_max)
-        category[mask] = 9
-            
-        #
-        # Store category and distance in the 'c' and 'i' field respectively.
-        #
-        i3_data[0]['c'] = category
-        i3_data[0]['i'] = distance
+            # Put top XX% in distance in color value 9.
+            mask = (km_distance > dist_max)
+            km_color[mask] = 9
 
-        i3_out.addMolecules(i3_data[0])
-
-        # Load the next block of data.
-        i3_data = [i3_readers[0].nextBlock()]
-
-    # Close output file
-    i3_out.close()
-
+            # Save results.
+            h5.addTrackData(km_color, i, "km_color")
+            h5.addTrackData(km_distance, i, "km_distance")
+        
     
 if (__name__ == "__main__"):
     
@@ -88,14 +70,12 @@ if (__name__ == "__main__"):
 
     parser.add_argument('--codebook', dest='codebook', type=str, required=True,
                         help = "A K-Means codebook.")
-    parser.add_argument('--basename', dest='basename', type=str, required=True,
-                        help = "The basename for the localization files.")
-    parser.add_argument('--output', dest='output', type=str, required=True,
-                        help = "The name of the file for the categorized localizations.")
+    parser.add_argument('--bin', dest='hdf5', type=str, required=True,
+                        help = "Localization HDF5 file.")
     parser.add_argument('--max_dist', dest='max_dist', type=float, required=False, default = 80.0,
                         help = "The maximum distance from a cluster center to keep as a percentile (default is 80%).")
     
     args = parser.parse_args()
 
     codebook = numpy.load(args.codebook)
-    KMeansClassifier(codebook, args.basename, args.output, max_distance = args.max_dist)
+    KMeansClassifier(codebook, args.hdf5, max_distance = args.max_dist)
--- storm_analysis/multi_plane/kmeans_measure_codebook.py
+++ storm_analysis/multi_plane/kmeans_measure_codebook.py
@@ -5,27 +5,41 @@ be run on data that comes from samples of sparse single color
 dyes. For example, one slide would be a Alexa-647 and another
 would be Biotium CF680.
 
-Hazen 09/17
+Multiple HDF5 files can be merged into a single file with
+sa_utilities.merge_hdf5.py
+
+Hazen 01/18
 """
 import numpy
 import scipy
 import scipy.cluster
 
-def KMeansMeasureVectors(height_fnames, n_categories):
+import storm_analysis.sa_library.sa_h5py as saH5Py
+
+def KMeansMeasureVectors(h5_filename, n_categories):
+
+    with saH5Py.SAH5Py(h5_filename) as h5:
+        assert h5.hasTracks(), "No tracking information."        
+        
+        # Load heights.
+        #
+        # Note: This loads all the tracks so this file is hopefully not
+        #       too large.
+        height_names = ["height"]
+        for i in range(1,h5.getNChannels()):
+            height_names.append(h5.getChannelPrefix(i) + "height")
+
+        tracks = h5.getTracks(fields = height_names)
+
+        # Create features array.
+        features = None
+        for i, elt in enumerate(height_names):
+
+            # Initialize features array if necessary.
+            if features is None:
+                features = numpy.zeros((tracks[elt].size, h5.getNChannels()))
+            features[:,i] = tracks[elt]
     
-    #
-    # Load height data from each file.
-    #
-    features = None
-    for i, ht_fname in enumerate(height_fnames):
-        h_data = numpy.load(ht_fname)
-
-        # Initialize features array if necessary.
-        if features is None:
-            features = h_data
-        else:
-            features = numpy.concatenate((features, h_data), axis = 0)
-
     # Normalize by total height.
     total = numpy.sum(features, axis = 1)
     for i in range(features.shape[0]):
@@ -46,8 +60,8 @@ if (__name__ == "__main__"):
     
     parser = argparse.ArgumentParser(description = 'Determine codebook for k-means classification.')
 
-    parser.add_argument('--heights', dest='heights', type=str, required=True, nargs = '*',
-                        help = "One or more numpy height files as created by multiplane/merge_heights.py.")
+    parser.add_argument('--bin', dest='hdf5', type=str, required=True,
+                        help = "Localization HDF5 file.")
     parser.add_argument('--ndyes', dest='ndyes', type=int, required=True,
                         help = "The number of different types of dyes to (attempt) to resolve.")
     parser.add_argument('--output', dest='output', type=str, required=True,
@@ -55,7 +69,7 @@ if (__name__ == "__main__"):
 
     args = parser.parse_args()
 
-    codebook = KMeansMeasureVectors(args.heights, args.ndyes)
+    codebook = KMeansMeasureVectors(args.hdf5, args.ndyes)
 
     print("Codebook:")
     print(codebook)
