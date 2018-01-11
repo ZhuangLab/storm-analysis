4548bde7351511952ccc16007db58458c6348cc3
Hazen Babcock <hbabcock@mac.com>
2018-01-10 20:27:30 -0500

    Update to work with HDF5 format data.

--- storm_analysis/multi_plane/plot_heights.py
+++ storm_analysis/multi_plane/plot_heights.py
@@ -2,59 +2,78 @@
 """
 Plot the channel height information from a data set.
 
-Hazen 08/17
+Hazen 01/18
 """
 
 import matplotlib
 import matplotlib.pyplot as pyplot
 import numpy
 
+import storm_analysis.sa_library.sa_h5py as saH5Py
 
-def plotHeights(ch_order, heights, min_sum = 0.0):
+def plotHeights(h5_filename, channel_order, min_sum = 0.0):
+    """
+    h5_filename - The name of the HDF5 file.
+    channel_order - A list of integers specifying the channel (wavelength) 
+                    order, for example [0, 2, 1, 3]
 
-    # Calculate height totals.
-    total_h = numpy.sum(heights, axis = 1)
+    Note: The HDF5 file must have tracking information.
+    """
+    with saH5Py.SAH5Py(h5_filename) as h5:
+        assert (h5.getNTracks() > 0), "No tracking information."
+        assert (len(channel_order) == h5.getNChannels())
 
-    # Remove localizations with small total height.
-    if (min_sum > 0.0):
-        mask = (total_h > min_sum)
+        # Figure out (ordered) height names.
+        fields = []
+        for elt in channel_order:
+            if (elt == 0):
+                fields.append("height")
+            else:
+                fields.append(h5.getChannelPrefix(elt) + "height")
+                
+        # Calculate total height.
+        for tracks in h5.tracksIterator(fields = fields):
+            total = numpy.zeros(tracks["height"].size)
 
-        total_h = total_h[mask]
-        heights = heights[mask,:]
-        
-    x = numpy.array(ch_order)
-    y = heights
+            for i in range(h5.getNChannels()):
+                total += tracks[fields[i]]
 
-    print(x.size, y.shape)
+            # Only load the first block of tracks.
+            break
 
-    fig = pyplot.figure()
+        # Plot up to 100 points.
+        n_pts = 0
+        x = numpy.arange(h5.getNChannels())
+        while(n_pts < 100):
+            if (total[n_pts] > min_sum):
+                y = []
+                for i in range(h5.getNChannels()):
+                    y.append(tracks[fields[i]][n_pts])
+                y = numpy.array(y)
+                pyplot.scatter(x + numpy.random.uniform(-0.2, 0.2),
+                               y/total[n_pts],
+                               color = "blue")
+            n_pts += 1
 
-    # Plot up to 100 points.
-    for i in range(100):
-        if (i < y.shape[0]):
-            pyplot.scatter(x + numpy.random.uniform(-0.2, 0.2), y[i,:]/total_h[i])
-
-    # Add labels.
-    pyplot.ylim((0.0, 1.0))
-    pyplot.xlabel("Channel Number")
-    pyplot.ylabel("Normalized Height (AU)")
-    pyplot.show()
+        # Add labels.
+        pyplot.ylim((0.0, 1.0))
+        pyplot.xlabel("Channel Number")
+        pyplot.ylabel("Normalized Height (AU)")
+        pyplot.show()
     
 
 if (__name__ == "__main__"):
 
     import argparse
 
-    parser = argparse.ArgumentParser(description = 'Plots normalized channel heights for diagnostic purposes.')
+    parser = argparse.ArgumentParser(description = 'Plots normalized channel heights.')
 
-    parser.add_argument('--heights', dest='heights', type=str, required=True,
-                        help = "The name of the heights file to plot.")
-    parser.add_argument('--order', dest='order', type=int, required=True, nargs = '*',
-                        help = "The channel order.")
-    parser.add_argument('--min_sum', dest='min_sum', type=float, required=False, default = 0.0,
-                        help = "The minimum heights sum. Default 0.0")
+    parser.add_argument('--bin', dest='hdf5', type=str, required=True,
+                        help = "Localization HDF5 file.")
+    parser.add_argument('--order', nargs = '*', dest='order', type=str, required=True,
+                        help = "The channel order by wavelength for example '2 1 0 3'.")
 
     args = parser.parse_args()
-
-    height_data = numpy.load(args.heights)
-    plotHeights(args.order, height_data, min_sum = args.min_sum)
+    
+    channel_order = list(map(int, args.order))
+    plotHeights(args.hdf5, channel_order)
