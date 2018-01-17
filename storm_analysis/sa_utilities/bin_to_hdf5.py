2821e94a68800d360a419eabbae7e3fbbfe867bb
Hazen Babcock <hbabcock@mac.com>
2018-01-17 15:29:41 -0500

    Make it a easier to convert bin files that don't have metadata.

--- storm_analysis/sa_utilities/bin_to_hdf5.py
+++ storm_analysis/sa_utilities/bin_to_hdf5.py
@@ -20,25 +20,40 @@ def BinToHDF5(bin_name, hdf5_name):
     if os.path.exists(hdf5_name):
         os.remove(hdf5_name)
 
-    with saH5Py.SAH5Py(hdf5_name) as h5:
+    with saH5Py.SAH5Py(hdf5_name, is_existing = False) as h5:
+
+        if True:
+            metadata = readinsight3.loadI3Metadata(bin_name)
+            assert (metadata is not None), "No metadata found for " + bin_name
         
-        metadata = readinsight3.loadI3Metadata(bin_name)
-        movie_xml = metadata.find("movie")
-        movie_x = int(movie_xml.find("movie_x").text)
-        movie_y = int(movie_xml.find("movie_y").text)
+            movie_xml = metadata.find("movie")
+            movie_x = int(movie_xml.find("movie_x").text)
+            movie_y = int(movie_xml.find("movie_y").text)
+            movie_l = int(movie_xml.find("movie_l").text)
+            hash_value = movie_xml.find("hash_value")
+            
+            params_xml = metadata.find("settings")
+            nm_per_pixel = float(params_xml.find("pixel_size").text)
+            
+        else:
+            # Fill this in to work around missing metadata.
+            movie_x = 256
+            movie_y = 256
+            movie_l = 10
+            hash_value = ""
+
+            params_xml = None
+            nm_per_pixel = 100.0
 
         # Set movie properties.
-        h5.setMovieProperties(movie_x, movie_y,
-                              int(movie_xml.find("movie_l").text),
-                              movie_xml.find("hash_value"))
+        h5.setMovieInformation(movie_x, movie_y, movie_l, hash_value)
 
         # Set pixel size.
-        params_xml = metadata.find("settings")
-        nm_per_pixel = float(params_xml.find("pixel_size").text)
         h5.setPixelSize(nm_per_pixel)
 
         # Set metadata.
-        h5.addMetadata(ElementTree.tostring(params_xml, 'unicode'))
+        if params_xml is not None:
+            h5.addMetadata(ElementTree.tostring(params_xml, 'unicode'))
 
         # Convert data.
         i3 = readinsight3.I3Reader(bin_name)
