2a8dbe4a04f242b9bd8c68c3ba63c0d152a29e88
Hazen Babcock <hbabcock@mac.com>
2018-03-18 21:35:44 -0400

    Update PSF FFT diagnostics to work better with jupyter notebooks.

--- storm_analysis/diagnostics/psf_fft/make_data.py
+++ storm_analysis/diagnostics/psf_fft/make_data.py
@@ -17,48 +17,54 @@ import storm_analysis.simulator.photophysics as photophysics
 import storm_analysis.simulator.psf as psf
 import storm_analysis.simulator.simulate as simulate
 
-import settings
+import storm_analysis.diagnostics.psf_fft.settings as settings
 
-index = 1
+def makeData():
 
-# Ideal camera movies.
-#
-if True:
-    for [bg, photons] in settings.photons:
-
-        wdir = "test_{0:02d}".format(index)
-        print(wdir)
-        if not os.path.exists(wdir):
-            os.makedirs(wdir)
-
-        bg_f = lambda s, x, y, i3 : background.UniformBackground(s, x, y, i3, photons = bg)
-        cam_f = lambda s, x, y, i3 : camera.Ideal(s, x, y, i3, settings.camera_offset)
-        pp_f = lambda s, x, y, i3 : photophysics.AlwaysOn(s, x, y, i3, photons)
-        psf_f = lambda s, x, y, i3 : psf.PupilFunction(s, x, y, i3, settings.pixel_size, settings.zmn)
-
-        sim = simulate.Simulate(background_factory = bg_f,
-                                camera_factory = cam_f,
-                                photophysics_factory = pp_f,
-                                psf_factory = psf_f,
-                                x_size = settings.x_size,
-                                y_size = settings.y_size)
+    index = 1
     
-        sim.simulate(wdir + "/test.dax", "grid_list.hdf5", settings.n_frames)
+    # Ideal camera movies.
+    #
+    if True:
+        for [bg, photons] in settings.photons:
+            
+            wdir = "test_{0:02d}".format(index)
+            print(wdir)
+            if not os.path.exists(wdir):
+                os.makedirs(wdir)
+
+            bg_f = lambda s, x, y, i3 : background.UniformBackground(s, x, y, i3, photons = bg)
+            cam_f = lambda s, x, y, i3 : camera.Ideal(s, x, y, i3, settings.camera_offset)
+            pp_f = lambda s, x, y, i3 : photophysics.AlwaysOn(s, x, y, i3, photons)
+            psf_f = lambda s, x, y, i3 : psf.PupilFunction(s, x, y, i3, settings.pixel_size, settings.zmn)
+
+            sim = simulate.Simulate(background_factory = bg_f,
+                                    camera_factory = cam_f,
+                                    photophysics_factory = pp_f,
+                                    psf_factory = psf_f,
+                                    x_size = settings.x_size,
+                                    y_size = settings.y_size)
+    
+            sim.simulate(wdir + "/test.dax", "grid_list.hdf5", settings.n_frames)
         
-        index += 1
+            index += 1
 
         
-# Create "peak_locations" file if needed.
-#
-if hasattr(settings, "peak_locations") and (settings.peak_locations is not None):
-    with saH5Py.SAH5Py("test_01/test_ref.hdf5") as h5:
-        locs = h5.getLocalizationsInFrame(0)
+    # Create "peak_locations" file if needed.
+    #
+    if hasattr(settings, "peak_locations") and (settings.peak_locations is not None):
+        with saH5Py.SAH5Py("test_01/test_ref.hdf5") as h5:
+            locs = h5.getLocalizationsInFrame(0)
         
-    if settings.peak_locations.endswith(".hdf5"):
-        saH5Py.saveLocalizations(settings.peak_locations, locs)
-    else:
-        numpy.savetxt(settings.peak_locations,
-                      numpy.transpose(numpy.vstack((locs['x'],
-                                                    locs['y'],
-                                                    locs['height'],
-                                                    locs['background']))))
+        if settings.peak_locations.endswith(".hdf5"):
+            saH5Py.saveLocalizations(settings.peak_locations, locs)
+        else:
+            numpy.savetxt(settings.peak_locations,
+                          numpy.transpose(numpy.vstack((locs['x'],
+                                                        locs['y'],
+                                                        locs['height'],
+                                                        locs['background']))))
+
+if (__name__ == "__main__"):
+    makeData()
+    
