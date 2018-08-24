c9c8de265bcc22d24c91a8c278964a042b5aa878
Hazen Babcock <hbabcock@mac.com>
2018-08-24 15:06:34 -0400

    Move cluster finding into dbscan_analysis.py. Add a test of DBSCAN clustering.

--- storm_analysis/dbscan/dbscan_analysis.py
+++ storm_analysis/dbscan/dbscan_analysis.py
@@ -1,43 +1,73 @@
 #!/usr/bin/env python
 """
-Does DBSCAN cluster analysis.
+DBSCAN cluster analysis functions.
 
-Hazen 01/12
+Hazen 08/18
 """
-
+import numpy
 import os
 
-import storm_analysis.dbscan.find_clusters as findClusters
-import storm_analysis.dbscan.cluster_stats as clusterStats
-import storm_analysis.dbscan.cluster_size as clusterSize
+import storm_analysis.dbscan.clusters_sa_h5py as clSAH5Py
+import storm_analysis.dbscan.dbscan_c as dbscanC
 
 
-def dbscanAnalysis(bin_file, channel, eps = 40, mc = 10, min_size = 50):
+def dbscanAnalysis(h5_name, eps = 40, mc = 10, min_size = 50):
+    """
+    Runs findClusters() and clusterStats() on a storm-analysis format HDF5
+    format file.
 
-    # save a record of the clustering parameters.
-    bin_dir = os.path.dirname(bin_file)
-    if (len(bin_dir) == 0):
-        bin_dir = "."
+    h5_name - The name of the HDF5 file.
+    eps - DBSCAN epsilon parameter (in nanometers).
+    mc - DBSCAN mc parameter.
+    min_size - Minimum size cluster for cluster statistics.
+
+    Note: This uses the default behavior for findClusters() which is to
+          ignore the z position and category.
+    """
+    findClusters(h5_name, eps, mc)
     
-    with open(bin_dir + "/dbscan.txt", "w") as fp:
-        fp.write("eps = " + str(eps) + "\n")
-        fp.write("mc = " + str(mc) + "\n")
-        fp.write("min_size = " + str(min_size) + "\n")
 
-    cl_bin_file = bin_file[:-8] + "clusters_list.bin"
+def findClusters(h5_name, eps, mc, ignore_z = True, ignore_category = True, z_factor = 1.0):
+    """
+    Perform DBSCAN clustering on an HDF5 localization file.
 
-    # find clusters
-    if True:
-        findClusters.findClusters(bin_file, cl_bin_file, eps, mc)
+    h5_name - The name of the HDF5 file.
+    eps - DBSCAN epsilon parameter (in nanometers).
+    mc - DBSCAN mc parameter.
+    ignore_z - Ignore localization z position when clustering.
+    ignore_category - Ignore localization category when clustering.
+    z_factor - Weighting of Z versus X/Y position. A value of 0.5 for
+               example will make the clustering 1/2 as sensitive to
+               Z position.
 
-    # cluster stats
-    if True:
-        clusterStats.clusterStats(cl_bin_file, min_size - 1)
+    Note: Because all the x/y/z location information must be loaded
+          into memory for the DBSCAN algorithm there is a limit to
+          the size of localization file that can be clustered.
+    """
+    with clSAH5Py.SAH5Clusters(h5_name) as cl_h5:
+        [x, y, z, c, cl_dict] = cl_h5.getDataForClustering(ignore_z = ignore_z)
 
-    # cluster size
-    if True:
-        clusterSize.clusterSize(cl_bin_file, cl_bin_file[:-8] + "size_list.bin")
+        if ignore_z:
+            print("Warning! Clustering without using localization z value!")        
 
+        # Perform analysis without regard to category.
+        if ignore_category:
+            print("Warning! Clustering without regard to category!")
+            c = numpy.zeros(c.size)
+
+        # Cluster the data.
+        labels = dbscanC.dbscan(x, y, z, c, eps, mc, z_factor = z_factor)
+
+        # Save the data.
+        cl_h5.addClusters(labels, cl_dict)
+
+        # Save clustering info.
+        info = "dbscan,eps,{0:0.3f},mc,{1:d}".format(eps,mc)
+        info += ",iz," + str(ignore_z)
+        info += ",ic," + str(ignore_category)
+        info += ",zf,{0:3f}".format(z_factor)
+        cl_h5.setClusteringInfo(info)
+                
 
 if (__name__ == "__main__"):
 
@@ -46,9 +76,7 @@ if (__name__ == "__main__"):
     parser = argparse.ArgumentParser(description = 'DBSCAN clustering following Ester, KDD-96, 1996')
 
     parser.add_argument('--bin', dest='mlist', type=str, required=True,
-                        help = "The name of the localizations input file. This is a binary file in Insight3 format.")
-    parser.add_argument('--channel', dest='channel', type=int, required=True,
-                        help = "Which channel (or category) to use for clustering.")
+                        help = "The name of the localizations input file. This is a HDF5 format localization file.")
     parser.add_argument('--eps', dest='epsilon', type=float, required=False, default=40,
                         help = "The DBSCAN epsilon parameters in nanometers. The default is 40nm.")
     parser.add_argument('--mc', dest='mc', type=int, required=False, default=10,
@@ -58,4 +86,4 @@ if (__name__ == "__main__"):
 
     args = parser.parse_args()
 
-    dbscanAnalysis(args.mlist, args.channel, args.epsilon, args.mc, args.min_size)
+    dbscanAnalysis(args.mlist, args.epsilon, args.mc, args.min_size)
--- storm_analysis/test/test_clustering.py
+++ storm_analysis/test/test_clustering.py
@@ -1,7 +1,54 @@
 #!/usr/bin/env python
+"""
+Tests for DBSCAN and Voronoi clustering.
+"""
+import numpy
 
 import storm_analysis
+import storm_analysis.sa_library.sa_h5py as saH5Py
 
+import storm_analysis.dbscan.clusters_sa_h5py as clSAH5Py
+import storm_analysis.dbscan.dbscan_analysis as dbscanAnalysis
+
+
+def test_dbscan_clustering_1():
+    numpy.random.seed(1)
+    
+    filename = "test_clusters_sa_h5py.hdf5"
+    h5_name = storm_analysis.getPathOutputTest(filename)
+    storm_analysis.removeFile(h5_name)
+
+    # Write tracks data.
+    category = numpy.zeros(10, dtype = numpy.int32)
+    x = 10.0 * numpy.arange(10)
+    y = 10.0 * numpy.arange(10)
+    z = numpy.zeros(10)
+
+    with saH5Py.SAH5Py(h5_name, is_existing = False) as h5:
+        h5.setMovieInformation(1,1,2,"")
+        h5.setPixelSize(1.0)
+        
+        for i in range(100):
+            tracks = {"category" : category,
+                      "x" : x + numpy.random.normal(scale = 0.1, size = 10),
+                      "y" : y + numpy.random.normal(scale = 0.1, size = 10),
+                      "z" : z + numpy.random.normal(scale = 0.1, size = 10)}
+            
+            h5.addTracks(tracks)
+
+    # Cluster data with DBSCAN.
+    dbscanAnalysis.findClusters(h5_name, 2.0, 10)
+
+    # Check clustering results.
+    with clSAH5Py.SAH5Clusters(h5_name) as cl_h5:
+        assert(cl_h5.getNClusters() == 9)
+        for index, cluster in cl_h5.clustersIterator(skip_unclustered = False):
+            for elt in ['x', 'y', 'z']:
+                assert(cluster[elt].size == 100)
+                dev = numpy.std(cluster[elt])
+                assert(dev > 0.085)
+                assert(dev < 0.115)
+        
 
 def _test_dbscan_clustering():
     
@@ -56,6 +103,6 @@ def _test_voronoi_clustering():
     
 
 if (__name__ == "__main__"):
-    _test_dbscan_clustering()
-    _test_voronoi_clustering()
+    test_dbscan_clustering_1()
+
     
