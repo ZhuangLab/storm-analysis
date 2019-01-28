10bd65a7666d5a25a3e359403717204be5885a98
Hazen Babcock <hbabcock@mac.com>
2019-01-28 13:41:56 -0500

    Remove PF/PSF calculations as we are now using the C libraries in pupilfn for this purpose.

--- storm_analysis/simulator/pf_math.c
+++ storm_analysis/simulator/pf_math.c
@@ -1,138 +1,20 @@
 /*
  * C library for doing some of heavy lifting of pupil function 
- * calculations. This includes calculating zernike modes and
- * all the Fourier transforms for displacing the pupil function
- * in z, calculating the PSF and OTF rescaling.
+ * calculations.
  *
- * Hazen 02/18
+ * Hazen 01/19
  */
 
 #include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
 
-#include <fftw3.h>
-
-typedef struct pfData
-{
-  int size;     /* The size of pupil function in X/Y in pixels. */
-  int sf_set;   /* Scaling factor was set. */
-
-  double *kz;   /* Z translation multiplier. */
-  double *sf;   /* OTF scaling factor. */
-
-  fftw_complex *pf;  
-  fftw_complex *fftw_pf;  /* Used by the FFT. */
-  fftw_complex *fftw_psf; /* Used by the FFT. */
-
-  fftw_plan pf_to_psf;
-  fftw_plan psf_to_pf;
-} pfData;
-
-void pfCleanup(pfData *);
-void pfGetPSF(pfData *, double *, double);
 int pfFactorial(int);
-pfData *pfInitialize(double *, int);
 double pfPreFac(int, int, int);
-void pfResetScaling(pfData *);
-void pfSetPF(pfData *, double *, double *);
-void pfSetScaling(pfData *, double *);
 double pfZernike(int, int, double, double);
 void pfZernikeGrid(double *, int, double, double, double, int, int);
 double pfZernikeRad(int, int, double);
 
-
-/*
- * pfCleanup()
- *
- * Free the pfData structure.
- */
-void pfCleanup(pfData *pf_data)
-{
-  free(pf_data->kz);
-  free(pf_data->sf);
-
-  fftw_free(pf_data->pf);
-  fftw_free(pf_data->fftw_pf);
-  fftw_free(pf_data->fftw_psf);
-
-  fftw_destroy_plan(pf_data->pf_to_psf);
-  fftw_destroy_plan(pf_data->psf_to_pf);
-
-  free(pf_data);
-}
-
-/*
- * pfGetPSF()
- *
- * Returns the OTF scaled PSF at the requested z value.
- *
- * psf - Pre-allocated storage for the PSF.
- * dz - The z offset in microns.
- */
-void pfGetPSF(pfData *pf_data, double *psf, double dz)
-{
-  int i;
-  double dd,r1,c1;
-
-  /* Translate PF in Z if necessary. */
-  if (dz != 0.0){
-    for(i=0;i<(pf_data->size*pf_data->size);i++){
-      dd = -pf_data->kz[i]*dz;
-      r1 = cos(dd);
-      c1 = -sin(dd);
-      pf_data->fftw_pf[i][0] = r1*pf_data->pf[i][0] - c1*pf_data->pf[i][1];
-      pf_data->fftw_pf[i][1] = r1*pf_data->pf[i][1] + c1*pf_data->pf[i][0];
-    }
-  }
-  else{
-    for(i=0;i<(pf_data->size*pf_data->size);i++){
-      pf_data->fftw_pf[i][0] = pf_data->pf[i][0];
-      pf_data->fftw_pf[i][1] = pf_data->pf[i][1];
-    }
-  }
-
-  /* FFT to real space. */
-  fftw_execute(pf_data->pf_to_psf);
-
-  /* Apply OTF scaling, if requested. */
-  if (pf_data->sf_set){
-    for(i=0;i<(pf_data->size*pf_data->size);i++){
-      r1 = pf_data->fftw_psf[i][0];
-      c1 = pf_data->fftw_psf[i][1];
-      pf_data->fftw_psf[i][0] = r1*r1 + c1*c1;
-      pf_data->fftw_psf[i][1] = 0.0;
-    }
-
-    /* Back to Fourier space. */
-    fftw_execute(pf_data->psf_to_pf);
-
-    /* Apply scaling. */
-    for(i=0;i<(pf_data->size*pf_data->size);i++){
-      pf_data->fftw_pf[i][0] = pf_data->fftw_pf[i][0] * pf_data->sf[i];
-      pf_data->fftw_pf[i][1] = pf_data->fftw_pf[i][1] * pf_data->sf[i];
-    }
-
-    /* And back again. */
-    fftw_execute(pf_data->pf_to_psf);
-
-    /* Calculate PSF. */
-    for(i=0;i<(pf_data->size*pf_data->size);i++){
-      r1 = pf_data->fftw_psf[i][0];
-      c1 = pf_data->fftw_psf[i][1];
-      psf[i] = sqrt(r1*r1 + c1*c1);
-    }
-  }
-  else{
-    
-    /* Calculate PSF. */
-    for(i=0;i<(pf_data->size*pf_data->size);i++){
-      r1 = pf_data->fftw_psf[i][0];
-      c1 = pf_data->fftw_psf[i][1];
-      psf[i] = r1*r1 + c1*c1;
-    }
-  }
-}
 
 /*
  * pfFactorial()
@@ -155,39 +37,6 @@ int pfFactorial(int n)
   return n_fac;
 }
 
-/*
- * pfInitialize()
- *
- * Create a pfData structure.
- *
- * kz - Vector to use for z offsets.
- * size - The size of the pupil function.
- */
-pfData *pfInitialize(double *kz, int size)
-{
-  int i;
-  pfData *pf_data;
-
-  pf_data = (pfData *)malloc(sizeof(pfData));
-  pf_data->size = size;
-  pf_data->sf_set = 0;
-
-  pf_data->kz = (double *)malloc(sizeof(double)*size*size);
-  pf_data->sf = (double *)malloc(sizeof(double)*size*size);
-
-  pf_data->pf = (fftw_complex *)fftw_malloc(sizeof(fftw_complex)*size*size);
-  pf_data->fftw_pf = (fftw_complex *)fftw_malloc(sizeof(fftw_complex)*size*size);
-  pf_data->fftw_psf = (fftw_complex *)fftw_malloc(sizeof(fftw_complex)*size*size);
-
-  pf_data->pf_to_psf = fftw_plan_dft_2d(size, size, pf_data->fftw_pf, pf_data->fftw_psf, FFTW_BACKWARD, FFTW_MEASURE);
-  pf_data->psf_to_pf = fftw_plan_dft_2d(size, size, pf_data->fftw_psf, pf_data->fftw_pf, FFTW_FORWARD, FFTW_MEASURE);
-
-  for(i=0;i<(size*size);i++){
-    pf_data->kz[i] = 2.0*M_PI*kz[i];
-  }
-
-  return pf_data;
-}
 
 /*
  * pfPreFac()
@@ -213,78 +62,6 @@ double pfPreFac(int m, int n, int k)
   return sign * n1/(d1 * d2 * d3);
 }
 
-/*
- * pfResetScaling()
- *
- * Turns off OTF scaling.
- */
-void pfResetScaling(pfData *pf_data)
-{
-  pf_data->sf_set = 0;
-}
-
-/*
- * pfSetPF()
- *
- * Set the pupil function.
- *
- * r_pf - The real part of the pupil function.
- * c_pf - The complex part of the pupil function.
- */
-void pfSetPF(pfData *pf_data, double *r_pf, double *c_pf)
-{
-  int i,j,k,l;
-  double norm;
-
-  norm = 1.0/((double)pf_data->size);
-
-  /* 
-   * Copy pupil function. 
-   * 
-   * Notes: 
-   *   1. We are messing with the sign so that the results will be
-   *      properly centered after the iFFT.
-   * 
-   *   2. We normalize the PF now so that we only have to do this
-   *      once.
-   */
-  for(i=0;i<pf_data->size;i++){
-    j = i * pf_data->size;
-    for(k=0;k<pf_data->size;k++){
-      l = j+k;
-      if(((i+k)%2)==0){
-	pf_data->pf[l][0] = r_pf[l] * norm;
-  	pf_data->pf[l][1] = c_pf[l] * norm;
-      }
-      else{
-	pf_data->pf[l][0] = -1.0*r_pf[l] * norm;
-	pf_data->pf[l][1] = -1.0*c_pf[l] * norm;
-      }
-    }
-  }
-}
-
-/* 
- * pfSetScaling()
- *
- * Set OTF scaling term.
- *
- * scaling_factor - The OTF scaling factor.
- */
-void pfSetScaling(pfData *pf_data, double *scaling_factor)
-{
-  int i;
-  double norm;
-
-  /* Normalize the OTF scaling now so that we only have to do this once. */
-  norm = 1.0/(((double)pf_data->size)*((double)pf_data->size));
-  
-  pf_data->sf_set = 1;
-
-  for(i=0;i<(pf_data->size*pf_data->size);i++){
-    pf_data->sf[i] = scaling_factor[i]*norm;
-  }
-}
 
 /*
  * pfZernike()
@@ -304,6 +81,7 @@ double pfZernike(int m, int n, double rho, double phi)
   if (m < 0) return pfZernikeRad(-m, n, rho) * sin(-m * phi);
   return pfZernikeRad(0, n, rho);
 }
+
 
 /*
  * pfZernikeGrid()
@@ -337,6 +115,7 @@ void pfZernikeGrid(double *grid, int gsize, double gcenter, double radius, doubl
     }
   }
 }
+
 
 /*
  * pfZernikeRad()
--- storm_analysis/simulator/pf_math_c.py
+++ storm_analysis/simulator/pf_math_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 """
-Simple Python interface to zernike.c
+Simple Python interface to pf_math.c
 
-Hazen 10/14
+Hazen 01/19
 """
 
 import ctypes
@@ -15,31 +15,6 @@ import storm_analysis.sa_library.loadclib as loadclib
 
 pf_math = loadclib.loadCLibrary("pf_math")
 
-pf_math.pfCleanup.argtypes = [ctypes.c_void_p]
-
-pf_math.pfGetPSF.argtypes = [ctypes.c_void_p,
-                             ndpointer(dtype=numpy.float64),
-                             ctypes.c_double]
-
-pf_math.pfInitialize.argtypes = [ndpointer(dtype=numpy.float64),
-                                 ctypes.c_int]
-pf_math.pfInitialize.restype = ctypes.c_void_p
-
-pf_math.pfResetScaling.argtypes = [ctypes.c_void_p]
-
-pf_math.pfSetPF.argtypes = [ctypes.c_void_p,
-                            ndpointer(dtype=numpy.float64),
-                            ndpointer(dtype=numpy.float64)]
-
-pf_math.pfSetScaling.argtypes = [ctypes.c_void_p,
-                                 ndpointer(dtype=numpy.float64)]
-                            
-pf_math.pfZernike.argtypes = [ctypes.c_int,
-                              ctypes.c_int,
-                              ctypes.c_double,
-                              ctypes.c_double]
-pf_math.pfZernike.restype = ctypes.c_double
-
 pf_math.pfZernikeGrid.argtypes = [ndpointer(dtype=numpy.float64),
                                   ctypes.c_int,
                                   ctypes.c_double,
@@ -47,58 +22,6 @@ pf_math.pfZernikeGrid.argtypes = [ndpointer(dtype=numpy.float64),
                                   ctypes.c_double,
                                   ctypes.c_int,
                                   ctypes.c_int]
-
-
-class PupilMath(object):
-    """
-    This speeds up the calculation of the PSF from the PF, which is what
-    takes 90-95% of the time in pupil function calculations.
-    """
-    def __init__(self, kz = None, **kwds):
-        super(PupilMath, self).__init__(**kwds)
-
-        assert (len(kz.shape) == 2), "kz must be two dimensional."
-        assert (kz.shape[0] == kz.shape[1]), "kz must be square."
-
-        self.pf_size = kz.shape[0]
-        assert ((self.pf_size%2) == 0), "pf size must be an even number."
-        
-        c_kz = numpy.ascontiguousarray(numpy.real(kz), dtype = numpy.float64)
-        self.pfm = pf_math.pfInitialize(c_kz, kz.shape[0])
-
-    def cleanup(self):
-        pf_math.pfCleanup(self.pfm)
-        self.pfm = None
-
-    def getPSF(self, z_vals):
-        psf = numpy.ascontiguousarray(numpy.zeros((len(z_vals), self.pf_size, self.pf_size)),
-                                      dtype = numpy.float64)
-
-        for i, z in enumerate(z_vals):
-            pf_math.pfGetPSF(self.pfm, psf[i,:,:], z)
-
-        return psf
-               
-    def setPF(self, pf):
-        assert(pf.shape[0] == self.pf_size), "PF has the wrong shape, axis 0."
-        assert(pf.shape[1] == self.pf_size), "PF has the wrong shape, axis 1."
-
-        #pf = numpy.fft.fftshift(pf)
-        pf_r = numpy.ascontiguousarray(numpy.real(pf), dtype = numpy.float64)
-        pf_c = numpy.ascontiguousarray(numpy.imag(pf), dtype = numpy.float64)
-        pf_math.pfSetPF(self.pfm, pf_r, pf_c)
-
-    def setScaling(self, scaling):
-        if scaling is None:
-            pf_math.pfResetScaling(self.pfm)
-
-        else:
-            assert(scaling.shape[0] == self.pf_size), "Scaling has the wrong shape, axis 0."
-            assert(scaling.shape[1] == self.pf_size), "Scaling has the wrong shape, axis 1."
-
-            scaling = numpy.fft.fftshift(scaling)
-            c_scaling = numpy.ascontiguousarray(scaling, dtype = numpy.float64)
-            pf_math.pfSetScaling(self.pfm, scaling)
 
 
 def zernikeGrid(np_array, scale, m, n, radius = None, center = None):
