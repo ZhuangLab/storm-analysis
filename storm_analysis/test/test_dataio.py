19e550ba1c636ac0ef4f593b425df110a72c05fb
Hazen Babcock <hbabcock@mac.com>
2019-03-25 18:26:08 -0400

    Add frameIterator() method to make it easier to step through the frames of a movie. Change averageFrames() to use frameIterator(). Add test of averageFrames() and frameIterator().

--- storm_analysis/test/test_dataio.py
+++ storm_analysis/test/test_dataio.py
@@ -93,7 +93,42 @@ def test_io_3():
     assert(ml == movie_l)
     assert(numpy.allclose(data, rd.loadAFrame(0)))
     
+def test_io_4():
+    """
+    Test averageFrames() and frameIterator()
+    """
+    movie_h = 50
+    movie_w = 40
+    movie_l = 10
+    
+    data = numpy.random.randint(0, 60000, (movie_h, movie_w)).astype(numpy.uint16)
 
+    movie_name = storm_analysis.getPathOutputTest("test_dataio.dax")
+
+    # Write dax movie.
+    wr = datawriter.inferWriter(movie_name)
+    for i in range(movie_l):
+        wr.addFrame(data)
+    wr.close()
+        
+    # Read & check.
+    rd = datareader.inferReader(movie_name)
+    [mw, mh, ml] = rd.filmSize()
+    
+    assert(mh == movie_h)
+    assert(mw == movie_w)
+    assert(ml == movie_l)
+
+    average = rd.averageFrames()
+    assert(numpy.allclose(data, average))
+        
+    for i in range(2,movie_l,2):
+        average = rd.averageFrames(start = i-2, end = i)
+        assert(numpy.allclose(data, average))
+
+    rd.close()
+
+    
 if (__name__ == "__main__"):
     test_io_1()
     test_io_2()
