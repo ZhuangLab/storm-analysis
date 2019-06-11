Fix initial background estimate to correct for RQE. Fix initial height estimate to correct for RQE. Fix calculation of peak foreground sum RQE correction. Update tests.

storm_analysis/test/test_multifit.py
@@ -378,7 +378,8 @@ def test_mfit_9():
             tf.save(image.astype(numpy.float32))
     
     sig = mfit.getPeakProperty("significance")
-    assert(abs((sig[1] - sig[0]) - (sig[2] - sig[1])) < 0.1)
+    sig = sig/sig[0]
+    assert(numpy.allclose(sig, numpy.arange(1,4)))
 
 def test_mfit_10():
     """
@@ -446,6 +447,51 @@ def test_mfit_11():
 
         mfit.cleanup(verbose = False)
 
+def test_mfit_12():
+    """
+    Test sensivity correction of peak significance calculation.
+    """
+    height = 10.0
+    rqe_term = 0.7
+    sigma = 1.5
+    x_size = 60
+    y_size = 120
+    background = numpy.zeros((x_size, y_size)) + 10.0
+    image = dg.drawGaussians((x_size, y_size),
+                             numpy.array([[30.0, 30.0, height, sigma, sigma],
+                                          [30.0, 60.0, height, sigma, sigma],
+                                          [30.0, 90.0, height, sigma, sigma]]))
+
+    image += background
+
+    rqe = numpy.ones_like(image)
+    rqe[:,:60] = rqe_term
+
+    image = image*rqe
+    background = background*rqe
+   
+    mfit = daoFitC.MultiFitter2D(rqe = rqe,
+                                 sensitivity_corrected = True,
+                                 sigma_range = [1.0, 2.0])
+    mfit.initializeC(image)
+    mfit.newImage(image)
+    mfit.newBackground(background)
+
+    peaks = {"x" : numpy.array([30.0, 60.0, 90.0]),
+             "y" : numpy.array([30.0, 30.0, 30.0]),
+             "z" : numpy.array([0.0, 0.0, 0.0]),
+             "sigma" : numpy.array([sigma, sigma, sigma])}
+
+    mfit.newPeaks(peaks, "finder")
+
+    if False:
+        with tifffile.TiffWriter("test_mfit_12.tif") as tf:
+            tf.save(image.astype(numpy.float32))
+    
+    sig = mfit.getPeakProperty("significance")
+    sig = sig/sig[0]
+    assert(numpy.allclose(sig, numpy.ones_like(sig), atol = 1.0e-2))
+
 
 if (__name__ == "__main__"):
     test_mfit_1()
@@ -459,4 +505,5 @@ if (__name__ == "__main__"):
     test_mfit_9()
     test_mfit_10()
     test_mfit_11()
+    test_mfit_12()
     