bb5c6f6985b6de61b412184522223275177a116e
Hazen Babcock <hbabcock@mac.com>
2019-10-30 15:21:51 -0400

    Code clean up. Change to usable as a module.

--- storm_analysis/simulator/emitters_in_clusters.py
+++ storm_analysis/simulator/emitters_in_clusters.py
@@ -2,76 +2,104 @@
 """
 Creates lists of molecules in clusters
 
-Hazen 09/17
+Hazen 10/19
 """
-import argparse
 import numpy
 import random
 
 import storm_analysis.sa_library.sa_h5py as saH5Py
 
-parser = argparse.ArgumentParser(description = "Create emitters in (possibly overlapping) clusters.")
 
-parser.add_argument('--bin', dest='hdf5', type=str, required=True,
-                    help = "The name of the HDF5 file to save the emitter locations, etc.")
-parser.add_argument('--ncl', dest='ncl', type=int, required=True,
-                    help = "The number of clusters.")
-parser.add_argument('--nlocs', dest='nlocs', type=int, required=True,
-                    help = "The number of localizations per cluster.")
-parser.add_argument('--dev', dest='dev', type=float, required=True,
-                    help = "Cluster standard deviation in pixels.")
-parser.add_argument('--sx', dest='sx', type=int, required=False, default=256,
-                    help = "Image x size in pixels, default is 256.")
-parser.add_argument('--sy', dest='sy', type=int, required=False, default=256,
-                    help = "Image y size in pixels, default is 256.")
-parser.add_argument('--z_start', dest='z_start', type=int, required=False, default=-0.5,
-                    help = "Starting value for z position in microns, default is -0.5um.")
-parser.add_argument('--z_stop', dest='z_stop', type=int, required=False, default=0.5,
-                    help = "Stopping value for z position in microns, default is 0.5um.")
+def emittersInClusters(h5_name, ncl, nlocs, dev, sigma = 1.5, sx = 256, sy = 256, z_start = -0.5, z_stop = 0.5):
+    """
+    h5_name - The name of the HDF5 file to save the emitter locations, etc.
+    ncl - The number of clusters.
+    nlocs - The number of localizations per cluster.
+    dev - Cluster standard deviation in pixels.
+    sigma - The sigma for the localizatoins, default is 1.5 pixels.
+    sx - Image x size in pixels, default is 256.
+    sy - Image y size in pixels, default is 256.
+    z_start - Starting value for z position in microns, default is -0.5um.
+    z_stop = Stopping value for z position in microns, default is 0.5um.
+    """
+                    
+    # First, create a list of cluster centers.
+    cl_centers = []
+    while (len(cl_centers) < ncl):
+        cx = random.uniform(0.0, sx)
+        cy = random.uniform(0.0, sy)
+        cz = random.uniform(z_start, z_stop)
 
-args = parser.parse_args()
+        # Don't keep the cluster if it is too close to the edge of the image.
+        if (cx < 2.0) or (cx > (sx - 2.0)):
+            continue
+        if (cy < 2.0) or (cy > (sy - 2.0)):
+            continue
 
-# First, create a list of cluster centers.
-cl_centers = []
-while (len(cl_centers) < args.ncl):
-    cx = random.uniform(0.0, args.sx)
-    cy = random.uniform(0.0, args.sy)
-    cz = random.uniform(args.z_start, args.z_stop)
+        cl_centers.append([cx, cy, cz])
 
-    # Don't keep the cluster if it is too close to the edge of the image.
-    if (cx < 2.0) or (cx > (args.sx - 2.0)):
-        continue
-    if (cy < 2.0) or (cy > (args.sy - 2.0)):
-        continue
+    # Next, create localizations for each cluster.
+    xp = None
+    yp = None
+    zp = None
+    for clc in cl_centers:
 
-    cl_centers.append([cx, cy, cz])
+        if xp is None:
+            xp = numpy.random.normal(scale = dev, size = nlocs) + clc[0]
+            yp = numpy.random.normal(scale = dev, size = nlocs) + clc[1]
 
-# Next, create localizations for each cluster.
-xp = None
-yp = None
-zp = None
-for clc in cl_centers:
+            # Z is in microns, we'll assume a 100nm pixel size.
+            zp = numpy.random.normal(scale = dev * 0.1, size = nlocs) + clc[2]
+        else:
+            xp = numpy.append(xp, numpy.random.normal(scale = dev, size = nlocs) + clc[0])
+            yp = numpy.append(yp, numpy.random.normal(scale = dev, size = nlocs) + clc[1])
+            zp = numpy.append(zp, numpy.random.normal(scale = dev * 0.1, size = nlocs) + clc[2])
 
-    if xp is None:
-        xp = numpy.random.normal(scale = args.dev, size = args.nlocs) + clc[0]
-        yp = numpy.random.normal(scale = args.dev, size = args.nlocs) + clc[1]
+    # Create a molecule list structure & save it.
+    peaks = {}
+    peaks["x"] = xp
+    peaks["y"] = yp
+    peaks["z"] = zp
+    peaks["xsigma"] = sigma*numpy.ones(xp.size)
+    peaks["ysigma"] = sigma*numpy.ones(yp.size)
 
-        # Z is in microns, we'll assume a 100nm pixel size.
-        zp = numpy.random.normal(scale = args.dev * 0.1, size = args.nlocs) + clc[2]
-    else:
-        xp = numpy.append(xp, numpy.random.normal(scale = args.dev, size = args.nlocs) + clc[0])
-        yp = numpy.append(yp, numpy.random.normal(scale = args.dev, size = args.nlocs) + clc[1])
-        zp = numpy.append(zp, numpy.random.normal(scale = args.dev * 0.1, size = args.nlocs) + clc[2])
+    # Save localizations.
+    with saH5Py.SAH5Py(h5_name, is_existing = False, overwrite = True) as h5:
+        h5.setMovieInformation(sx, sy, 1, "")
+        h5.addLocalizations(peaks, 0)
 
-# Create a molecule list structure & save it.
-peaks = {}
-peaks["x"] = xp
-peaks["y"] = yp
-peaks["z"] = zp
-peaks["xsigma"] = 1.5*numpy.ones(xp.size)
-peaks["ysigma"] = 1.5*numpy.ones(yp.size)
 
-# Save localizations.
-with saH5Py.SAH5Py(args.hdf5, is_existing = False, overwrite = True) as h5:
-    h5.setMovieInformation(args.sx, args.sy, 1, "")
-    h5.addLocalizations(peaks, 0)
+if (__name__ == "__main__"):
+    import argparse
+
+    parser = argparse.ArgumentParser(description = "Create emitters in (possibly overlapping) clusters.")
+
+    parser.add_argument('--bin', dest='hdf5', type=str, required=True,
+                        help = "The name of the HDF5 file to save the emitter locations, etc.")
+    parser.add_argument('--ncl', dest='ncl', type=int, required=True,
+                        help = "The number of clusters.")
+    parser.add_argument('--nlocs', dest='nlocs', type=int, required=True,
+                        help = "The number of localizations per cluster.")
+    parser.add_argument('--dev', dest='dev', type=float, required=True,
+                        help = "Cluster standard deviation in pixels.")
+    parser.add_argument('--sx', dest='sx', type=int, required=False, default=256,
+                        help = "Image x size in pixels, default is 256.")
+    parser.add_argument('--sy', dest='sy', type=int, required=False, default=256,
+                        help = "Image y size in pixels, default is 256.")
+    parser.add_argument('--z_start', dest='z_start', type=int, required=False, default=-0.5,
+                        help = "Starting value for z position in microns, default is -0.5um.")
+    parser.add_argument('--z_stop', dest='z_stop', type=int, required=False, default=0.5,
+                        help = "Stopping value for z position in microns, default is 0.5um.")
+
+    args = parser.parse_args()
+
+    emittersInClusters(args.hdf5,
+                       args.ncl,
+                       args.nlocs,
+                       args.dev,
+                       sx = args.sx,
+                       sy = args.sy,
+                       z_start = args.z_start,
+                       z_stop = args.z_stop)
+
+    
--- storm_analysis/simulator/emitters_on_lines.py
+++ storm_analysis/simulator/emitters_on_lines.py
@@ -4,12 +4,12 @@ Creates lists of emitters on randomly oriented lines.
 
 Hazen 08/17
 """
-import argparse
 import math
 import numpy
 import random
 
 import storm_analysis.sa_library.sa_h5py as saH5Py
+
 
 class Line(object):
 
@@ -33,84 +33,113 @@ class Line(object):
                 self.z_pos]
 
 
-parser = argparse.ArgumentParser(description = "Create randomly oriented lines of emitters.")
-
-parser.add_argument('--bin', dest='hdf5', type=str, required=True,
-                    help = "The name of the HDF5 file to save the emitter locations, etc.")
-parser.add_argument('--nlines', dest='nlines', type=int, required=True,
-                    help = "The number of lines.")
-parser.add_argument('--nemitters', dest='nemitters', type=int, required=True,
-                    help = "The number of emitters.")
-parser.add_argument('--sx', dest='sx', type=int, required=False, default=256,
-                    help = "Image x size in pixels, default is 256.")
-parser.add_argument('--sy', dest='sy', type=int, required=False, default=256,
-                    help = "Image y size in pixels, default is 256.")
-parser.add_argument('--maxl', dest='maxl', type=int, required=False, default=200,
-                    help = "Maximum line length in pixels, default is 200.")
-parser.add_argument('--minl', dest='minl', type=int, required=False, default=20,
-                    help = "Minimum line length in pixels, default is 20.")
-parser.add_argument('--start_a', dest='start_a', type=int, required=False, default=0,
-                    help = "Starting value for angular range, default is 0 degrees.")
-parser.add_argument('--stop_a', dest='stop_a', type=int, required=False, default=360,
-                    help = "Stopping value for angular range, default is 360 degrees.")
-parser.add_argument('--z_start', dest='z_start', type=int, required=False, default=-0.5,
-                    help = "Starting value for z position, default is -0.5um.")
-parser.add_argument('--z_stop', dest='z_stop', type=int, required=False, default=0.5,
-                    help = "Stopping value for z position, default is 0.5um.")
-        
-args = parser.parse_args()
-
-# Create line segments.
-lines = []
-for i in range(args.nlines):
-    lines.append(Line(x_pos = random.uniform(0.0, args.sx),
-                      y_pos = random.uniform(0.0, args.sy),
-                      z_pos = random.uniform(args.z_start, args.z_stop),
-                      angle = random.uniform(args.start_a, args.stop_a),
-                      length = random.uniform(args.minl, args.maxl)))
-
-# Generate emitter locations.
-peaks = {}
-peaks["x"] = numpy.zeros(args.nemitters)
-peaks["y"] = numpy.zeros(args.nemitters)
-peaks["z"] = numpy.zeros(args.nemitters)
-peaks["xsigma"] = 1.5*numpy.ones(args.nemitters)
-peaks["ysigma"] = 1.5*numpy.ones(args.nemitters)
-
-i = 0
-printed = False
-while (i < args.nemitters):
-
-    if((i%10000) == 0):
-        if not printed:
-            print("Adding point", i)
-            printed = True
-    else:
-        printed = False
-
-    # Get a random line.
-    line = random.choice(lines)
-
-    # Get a random point on the line.
-    pnt = line.getLocation(random.uniform(0.0, args.maxl))
-
-    # Record location if valid.
-    if pnt is not None:
-        if (pnt[0] < 0.0) or (pnt[0] > args.sx):
-            continue
-        if (pnt[1] < 0.0) or (pnt[1] > args.sy):
-            continue
-        
-        peaks['x'][i] = pnt[0]
-        peaks['y'][i] = pnt[1]
-        peaks['z'][i] = pnt[2]
-
-        i += 1
-        
-# Save localizations.
-with saH5Py.SAH5Py(args.hdf5, is_existing = False, overwrite = True) as h5:
-    h5.setMovieInformation(args.sx, args.sy, 1, "")
-    h5.addLocalizations(peaks, 0)
-
-
+def emittersOnLines(h5_name, nlines, nemitters, sx = 256, sy = 256, maxl = 200, minl = 20, start_a = 0, stop_a = 360, z_start = -0.5, z_stop = 0.5):
+    """
+    h5_name - The name of the HDF5 file to save the emitter locations, etc.
+    nlines - The number of lines.
+    nemitters - The number of emitters.
+    sx - Image x size in pixels, default is 256.
+    sy - Image y size in pixels, default is 256.
+    maxl - Maximum line length in pixels, default is 200.
+    minl - Minimum line length in pixels, default is 20.
+    start_a - Starting value for angular range, default is 0 degrees.
+    stop_a - Stopping value for angular range, default is 360 degrees.
+    z_start - Starting value for z position, default is -0.5um.
+    z_stop - Stopping value for z position, default is 0.5um.
+    """
     
+    # Create line segments.
+    lines = []
+    for i in range(nlines):
+        lines.append(Line(x_pos = random.uniform(0.0, sx),
+                          y_pos = random.uniform(0.0, sy),
+                          z_pos = random.uniform(z_start, z_stop),
+                          angle = random.uniform(start_a, stop_a),
+                          length = random.uniform(minl, maxl)))
+
+    # Generate emitter locations.
+    peaks = {}
+    peaks["x"] = numpy.zeros(nemitters)
+    peaks["y"] = numpy.zeros(nemitters)
+    peaks["z"] = numpy.zeros(nemitters)
+    peaks["xsigma"] = 1.5*numpy.ones(nemitters)
+    peaks["ysigma"] = 1.5*numpy.ones(nemitters)
+
+    i = 0
+    printed = False
+    while (i < nemitters):
+
+        if((i%10000) == 0):
+            if not printed:
+                print("Adding point", i)
+                printed = True
+        else:
+            printed = False
+
+        # Get a random line.
+        line = random.choice(lines)
+
+        # Get a random point on the line.
+        pnt = line.getLocation(random.uniform(0.0, maxl))
+
+        # Record location if valid.
+        if pnt is not None:
+            if (pnt[0] < 0.0) or (pnt[0] > sx):
+                continue
+            if (pnt[1] < 0.0) or (pnt[1] > sy):
+                continue
+        
+            peaks['x'][i] = pnt[0]
+            peaks['y'][i] = pnt[1]
+            peaks['z'][i] = pnt[2]
+
+            i += 1
+        
+    # Save localizations.
+    with saH5Py.SAH5Py(h5_name, is_existing = False, overwrite = True) as h5:
+        h5.setMovieInformation(sx, sy, 1, "")
+        h5.addLocalizations(peaks, 0)
+
+
+if (__name__ == "__main__"):
+    import argparse
+    
+    parser = argparse.ArgumentParser(description = "Create randomly oriented lines of emitters.")
+
+    parser.add_argument('--bin', dest='hdf5', type=str, required=True,
+                        help = "The name of the HDF5 file to save the emitter locations, etc.")
+    parser.add_argument('--nlines', dest='nlines', type=int, required=True,
+                        help = "The number of lines.")
+    parser.add_argument('--nemitters', dest='nemitters', type=int, required=True,
+                        help = "The number of emitters.")
+    parser.add_argument('--sx', dest='sx', type=int, required=False, default=256,
+                        help = "Image x size in pixels, default is 256.")
+    parser.add_argument('--sy', dest='sy', type=int, required=False, default=256,
+                        help = "Image y size in pixels, default is 256.")
+    parser.add_argument('--maxl', dest='maxl', type=int, required=False, default=200,
+                        help = "Maximum line length in pixels, default is 200.")
+    parser.add_argument('--minl', dest='minl', type=int, required=False, default=20,
+                        help = "Minimum line length in pixels, default is 20.")
+    parser.add_argument('--start_a', dest='start_a', type=int, required=False, default=0,
+                        help = "Starting value for angular range, default is 0 degrees.")
+    parser.add_argument('--stop_a', dest='stop_a', type=int, required=False, default=360,
+                        help = "Stopping value for angular range, default is 360 degrees.")
+    parser.add_argument('--z_start', dest='z_start', type=int, required=False, default=-0.5,
+                        help = "Starting value for z position, default is -0.5um.")
+    parser.add_argument('--z_stop', dest='z_stop', type=int, required=False, default=0.5,
+                        help = "Stopping value for z position, default is 0.5um.")
+        
+    args = parser.parse_args()
+
+    emittersOnLines(args.hdf5,
+                    args.nlines,
+                    argls.nemitters,
+                    sx = args.sx,
+                    sy = args.sy,
+                    maxl = args.maxl,
+                    minl = args.minl,
+                    start_a = args.start_a,
+                    stop_a = args.stop_a,
+                    z_start = args.z_start,
+                    z_stop = args.z_stop)
+
