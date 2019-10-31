debac8f99d3455fc025598fb93f2d610fa60afc8
Hazen Babcock <hbabcock@mac.com>
2019-10-30 20:48:10 -0400

    Remove subprocess.call().

--- storm_analysis/diagnostics/drift_correction/configure.py
+++ storm_analysis/diagnostics/drift_correction/configure.py
@@ -6,15 +6,12 @@ Note: This test takes approximately 20-30 minutes.
 
 Hazen 01/18
 """
-import inspect
 import numpy
-import os
-import shutil
-import subprocess
 
-import storm_analysis
 import storm_analysis.sa_library.parameters as parameters
-import storm_analysis.sa_library.sa_h5py as saH5Py
+
+import storm_analysis.simulator.emitters_in_clusters as emittersInClusters
+import storm_analysis.simulator.emitters_on_lines as emittersOnLines
 
 import storm_analysis.diagnostics.drift_correction.settings as settings
 
@@ -51,7 +48,8 @@ def testingParameters():
     params.setAttr("z_correction", "int", 1)
 
     return params
-    
+
+
 def configure():
     # Create parameters file for analysis.
     #
@@ -62,24 +60,21 @@ def configure():
     # Create localizations in clusters file.
     #
     print("Creating clustered localizations file.")
-    sim_path = os.path.dirname(inspect.getfile(storm_analysis)) + "/simulator/"
-    subprocess.call(["python", sim_path + "emitters_in_clusters.py",
-                     "--bin", "clusters_list.hdf5",
-                     "--dev", "1.0",
-                     "--ncl", "50",
-                     "--nlocs", "200",
-                     "--sx", str(settings.x_size),
-                     "--sy", str(settings.y_size)])
+    emittersInClusters.emittersInClusters("clusters_list.hdf5",
+                                          50,
+                                          200,
+                                          1.0,
+                                          sx = settings.x_size,
+                                          sy = settings.y_size)
 
     # Create localizations on lines file.
     #
     print("Creating lines localizations file.")
-    subprocess.call(["python", sim_path + "emitters_on_lines.py",
-                     "--bin", "lines_list.hdf5",
-                     "--nlines", "50",
-                     "--nemitters", "100000",
-                     "--sx", str(settings.x_size),
-                     "--sy", str(settings.y_size)])
+    emittersOnLines.emittersOnLines("lines_list.hdf5",
+                                    50,
+                                    100000,
+                                    sx = settings.x_size,
+                                    sy = settings.y_size)
 
     # Create drift file. This is used in the simulations to displace
     # the localizations.
--- storm_analysis/diagnostics/frc/configure.py
+++ storm_analysis/diagnostics/frc/configure.py
@@ -4,11 +4,7 @@ Configure folder for FRC testing.
 
 Hazen 01/18
 """
-import inspect
-import os
-import subprocess
-
-import storm_analysis
+import storm_analysis.simulator.emitters_in_clusters as emittersInClusters
 
 import storm_analysis.diagnostics.frc.settings as settings
 
@@ -17,16 +13,13 @@ def configure():
     # Create localizations in clusters file.
     #
     print("Creating clustered localizations file.")
-    sim_path = os.path.dirname(inspect.getfile(storm_analysis)) + "/simulator/"
-    subprocess.call(["python", sim_path + "emitters_in_clusters.py",
-                     "--bin", "clusters_list.hdf5",
-                     "--dev", "1.0",
-                     "--ncl", "50",
-                     "--nlocs", "2000",
-                     "--sx", str(settings.x_size),
-                     "--sy", str(settings.y_size)])
+    emittersInClusters.emittersInClusters("clusters_list.hdf5",
+                                          50,
+                                          2000,
+                                          1.0,
+                                          sx = settings.x_size,
+                                          sy = settings.y_size)
 
 
 if (__name__ == "__main__"):
     configure()
-    
