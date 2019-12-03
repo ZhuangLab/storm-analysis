1c0e73c2a0bd87ba9bdd54e6c3f0bc6d71358dd7
Hazen Babcock <hbabcock@mac.com>
2019-12-03 17:11:00 -0500

    Also return localization significance and fitting iterations.

--- storm_analysis/spliner/find_peaks_decon.py
+++ storm_analysis/spliner/find_peaks_decon.py
@@ -298,7 +298,7 @@ def initFindAndFit(parameters, settings_name):
                                             parameters = parameters)
 
     # Specify which properties we want from the analysis.
-    properties = ["background", "error", "height", "sum", "x", "y", "z"]
+    properties = ["background", "error", "height", "iterations", "significance", "sum", "x", "y", "z"]
     
     return fitting.PeakFinderFitter(peak_finder = finder,
                                     peak_fitter = fitter,
