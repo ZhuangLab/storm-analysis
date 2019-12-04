c08ab7134e93d276a1d7e560ffae90aa92c476a2
Hazen Babcock <hbabcock@mac.com>
2019-12-04 13:56:24 -0500

    Update test to correct for change in margin size.

--- storm_analysis/test/test_cs_decon_utilities.py
+++ storm_analysis/test/test_cs_decon_utilities.py
@@ -12,7 +12,7 @@ def test_cs_utils_1():
     # Create test image with some non-zero data.
     test_image = numpy.zeros((10,10,3))
 
-    test_image[0,0,0] = 1.0
+    test_image[1,1,0] = 1.0
     
     test_image[5,5,0] = 1.0
     test_image[5,5,1] = 2.0
@@ -25,8 +25,8 @@ def test_cs_utils_1():
     assert(counts == 3)
 
     peaks = csDeconUtilsC.getPeaks(test_image, 0.1, 0)
-    assert(numpy.allclose(peaks[:,1], numpy.array([0, 5, 7])))
-    assert(numpy.allclose(peaks[:,2], numpy.array([0, 5, 7])))
+    assert(numpy.allclose(peaks[:,1], numpy.array([1, 5, 7])))
+    assert(numpy.allclose(peaks[:,2], numpy.array([1, 5, 7])))
 
 
 if (__name__ == "__main__"):
