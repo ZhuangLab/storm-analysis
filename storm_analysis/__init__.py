796540b4fb27bd825bbfdb6ad578e78594f1fd30
Hazen Babcock <hbabcock@mac.com>
2023-02-26 14:49:29 -0500

    Update version.

--- storm_analysis/__init__.py
+++ storm_analysis/__init__.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as pyplot
 class SAException(Exception):
     pass
 
-__version__ = "2020.07.01"
+__version__ = "2023.02.26"
 
 # Maybe there is a builtin function that does this??
 def asciiString(value):
